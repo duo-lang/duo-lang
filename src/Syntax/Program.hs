951293b503995f9b79d2e26addab41762cd2a292
BinderDavid <dbinder1989@googlemail.com>
2020-12-07 22:15:54 +0100

    Add dataDecls to environment

--- src/Syntax/Program.hs
+++ src/Syntax/Program.hs
@@ -23,13 +23,15 @@ data Environment = Environment
   { prdEnv :: Map FreeVarName (Term Prd ())
   , cnsEnv :: Map FreeVarName (Term Cns ())
   , typEnv :: Map TypeIdentifierName TypeScheme
+  , declEnv :: [DataDecl]
   }
 
 instance Semigroup Environment where
-  (Environment prdEnv1 cnsEnv1 typEnv1) <> (Environment prdEnv2 cnsEnv2 typEnv2) =
+  (Environment prdEnv1 cnsEnv1 typEnv1 declEnv1) <> (Environment prdEnv2 cnsEnv2 typEnv2 declEnv2) =
     Environment { prdEnv = M.union prdEnv1 prdEnv2
                 , cnsEnv = M.union cnsEnv1 cnsEnv2
                 , typEnv = M.union typEnv1 typEnv2
+                , declEnv = declEnv1 ++ declEnv2
                 }
 
 instance Monoid Environment where
@@ -37,10 +39,11 @@ instance Monoid Environment where
     { prdEnv = M.empty
     , cnsEnv = M.empty
     , typEnv = M.empty
+    , declEnv = []
     }
 
 insertDecl :: Declaration () -> Environment -> Environment
-insertDecl (PrdDecl v t) env@Environment { prdEnv } = env { prdEnv = M.insert v t prdEnv }
-insertDecl (CnsDecl v t) env@Environment { cnsEnv } = env { cnsEnv = M.insert v t cnsEnv }
-insertDecl (TypDecl n t) env@Environment { typEnv } = env { typEnv = M.insert n t typEnv }
-insertDecl (DataDecl _) env = env
+insertDecl (PrdDecl v t)  env@Environment { prdEnv }  = env { prdEnv  = M.insert v t prdEnv }
+insertDecl (CnsDecl v t)  env@Environment { cnsEnv }  = env { cnsEnv  = M.insert v t cnsEnv }
+insertDecl (TypDecl n t)  env@Environment { typEnv }  = env { typEnv  = M.insert n t typEnv }
+insertDecl (DataDecl dcl) env@Environment { declEnv } = env { declEnv = dcl : declEnv }
