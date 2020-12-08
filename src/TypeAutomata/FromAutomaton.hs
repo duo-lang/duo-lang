b1d83e9e670ee99956a88c36dfcb244368a6bebd
BinderDavid <dbinder1989@googlemail.com>
2020-12-08 01:15:37 +0100

    Start simplification

--- src/TypeAutomata/FromAutomaton.hs
+++ src/TypeAutomata/FromAutomaton.hs
@@ -23,18 +23,28 @@ import Data.Graph.Inductive.Query.DFS (dfs)
 -- Type automata -> Target types
 --------------------------------------------------------------------------
 
+data AutToTypeState = AutToTypeState { tvMap :: Map Node (Set TVar)
+                                     , graph :: TypeGr
+                                     , cache :: Set Node
+                                     }
+
+type AutToTypeM a = Reader AutToTypeState a
+
 autToType :: TypeAutDet -> TypeScheme
 autToType aut@TypeAut{..} =
   let
     mp = getFlowAnalysisMap aut
-    monotype = runReader (autToTypeReader mp (runIdentity ta_starts)) (ta_gr, S.empty)
+    startState = AutToTypeState mp ta_gr S.empty
+    monotype = runReader (nodeToType (runIdentity ta_starts)) startState
     tvars = S.toList $ S.unions (M.elems mp)
   in
     TypeScheme tvars monotype
 
-autToTypeReader :: Map Node (Set TVar) -> Node -> Reader (TypeGr, Set Node) TargetType
-autToTypeReader tvMap i = do
-  (gr,cache) <- ask
+
+
+nodeToType :: Node -> AutToTypeM TargetType
+nodeToType i = do
+  (AutToTypeState tvMap gr cache) <- ask
   let (_,_,(pol,HeadCons datSet codatSet),outs) = context gr i
   let (maybeDat,maybeCodat) = (S.toList <$> datSet, S.toList <$> codatSet)
   -- highestIndex :: DataOrCodata -> XtorName -> PrdOrCns -> Int
@@ -42,19 +52,19 @@ autToTypeReader tvMap i = do
   if i `S.member` cache
     then return $ TTyRVar (MkRVar ("r" ++ show i))
     else do
-      resType <- local (\(gr0,cache0) ->
-          (delEdges [(i,n) | n <- suc gr i, i `elem` dfs [n] gr] gr0
-          ,S.insert i cache0)) $ do
+      resType <- local (\(AutToTypeState mp gr0 cache0) -> AutToTypeState { tvMap = mp
+                                                                          , graph = delEdges [(i,n) | n <- suc gr i, i `elem` dfs [n] gr] gr0
+                                                                          , cache = S.insert i cache0 }) $ do
         let varL = TTyTVar <$> (S.toList $ fromJust $ M.lookup i tvMap)
         datL <- case maybeDat of
           {Nothing -> return [] ;
           Just xtors -> do
             sig <- forM xtors $ \xt -> do
               prdTypes <- forM [0..highestIndex Data xt Prd] $ \j -> do
-                typs <- sequence [autToTypeReader tvMap n | (EdgeSymbol Data xt' Prd j', n) <- outs, xt == xt', j == j']
+                typs <- sequence [nodeToType n | (EdgeSymbol Data xt' Prd j', n) <- outs, xt == xt', j == j']
                 return $ unionOrInter (applyVariance Data Prd pol) typs
               cnsTypes <- forM [0..highestIndex Data xt Cns] $ \j -> do
-                typs <- sequence [autToTypeReader tvMap n | (EdgeSymbol Data xt' Cns j', n) <- outs, xt == xt', j == j']
+                typs <- sequence [nodeToType n | (EdgeSymbol Data xt' Cns j', n) <- outs, xt == xt', j == j']
                 return $ unionOrInter (applyVariance Data Cns pol) typs
               return (MkXtorSig xt (Twice prdTypes cnsTypes))
             return [TTySimple Data sig]}
@@ -63,10 +73,10 @@ autToTypeReader tvMap i = do
           Just xtors -> do
             sig <- forM xtors $ \xt -> do
               prdTypes <- forM [0..highestIndex Codata xt Prd] $ \j -> do
-                typs <- sequence [autToTypeReader tvMap n | (EdgeSymbol Codata xt' Prd j', n) <- outs, xt == xt', j == j']
+                typs <- sequence [nodeToType n | (EdgeSymbol Codata xt' Prd j', n) <- outs, xt == xt', j == j']
                 return $ unionOrInter (applyVariance Codata Prd pol) typs
               cnsTypes <- forM [0..highestIndex Codata xt Cns] $ \j -> do
-                typs <- sequence [autToTypeReader tvMap n | (EdgeSymbol Codata xt' Cns j', n) <- outs, xt == xt', j == j']
+                typs <- sequence [nodeToType n | (EdgeSymbol Codata xt' Cns j', n) <- outs, xt == xt', j == j']
                 return $ unionOrInter (applyVariance Codata Cns pol) typs
               return (MkXtorSig xt (Twice prdTypes cnsTypes))
             return [TTySimple Codata sig]}
