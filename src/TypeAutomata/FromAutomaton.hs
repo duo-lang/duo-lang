44236d6ca3fe208e032824322f6f7671f00b958c
BinderDavid <dbinder1989@googlemail.com>
2020-12-08 02:25:15 +0100

    Some simplification

--- src/TypeAutomata/FromAutomaton.hs
+++ src/TypeAutomata/FromAutomaton.hs
@@ -9,6 +9,7 @@ import TypeAutomata.FlowAnalysis
 import Control.Monad.Reader
 import Data.Maybe (fromJust)
 
+import Data.List (sortBy, groupBy)
 import Data.Functor.Identity
 import Data.Set (Set)
 import qualified Data.Set as S
@@ -40,44 +41,80 @@ autToType aut@TypeAut{..} =
   in
     TypeScheme tvars monotype
 
+visitNode :: Node -> AutToTypeState -> AutToTypeState
+visitNode i aut@AutToTypeState { graph, cache } =
+  aut { graph = delEdges [(i,n) | n <- suc graph i, i `elem` dfs [n] graph] graph
+      , cache = S.insert i cache }
 
+checkCache :: Node -> AutToTypeM Bool
+checkCache i = do
+  cache <- asks cache
+  return (i `S.member` cache)
+
+nodeToTVars :: Node -> AutToTypeM [TargetType]
+nodeToTVars i = do
+  tvMap <- asks tvMap
+  return (TTyTVar <$> (S.toList $ fromJust $ M.lookup i tvMap))
+
+nodeToOuts :: Node -> AutToTypeM [(EdgeLabel, Node)]
+nodeToOuts i = do
+  gr <- asks graph
+  let (_,_,_,outs) = context gr i
+  return outs
+
+filterSortOuts :: [(EdgeLabel, Node)] -> DataCodata -> XtorName -> PrdCns -> [[Node]]
+filterSortOuts outs dc xt pc =
+  let
+    filtereds = [ (el, n) | (el@(EdgeSymbol dc' xt' pc' _), n) <- outs, dc == dc', xt == xt', pc == pc' ]
+    sortFun (EdgeSymbol _ _ _ j1,_) (EdgeSymbol _ _ _ j2,_) = j1 `compare` j2
+    sorteds = sortBy sortFun filtereds
+    groupFun (EdgeSymbol _ _ _ j1,_) (EdgeSymbol _ _ _ j2,_) = j1 == j2
+    groupeds = groupBy groupFun sorteds
+  in
+    (fmap . fmap) snd $ groupeds
+
+bar :: [[Node]] -> DataCodata -> PrdCns -> Polarity -> AutToTypeM [TargetType]
+bar nodes dc pc pol = do
+  forM nodes $ \ns -> do
+    typs <- forM ns $ \n -> do
+      nodeToType n
+    return $ unionOrInter (applyVariance dc pc pol) typs
 
 nodeToType :: Node -> AutToTypeM TargetType
 nodeToType i = do
-  (AutToTypeState tvMap gr cache) <- ask
-  let (_,_,(pol,HeadCons datSet codatSet),outs) = context gr i
-  let (maybeDat,maybeCodat) = (S.toList <$> datSet, S.toList <$> codatSet)
-  -- highestIndex :: DataOrCodata -> XtorName -> PrdOrCns -> Int
-  let highestIndex s xt pc = maximum ((-1) : [j | (EdgeSymbol _ _ _ j, _) <- filter (\(EdgeSymbol s' xt' pc' _, _) -> s==s' && xt==xt' && pc==pc') outs])
-  if i `S.member` cache
-    then return $ TTyRVar (MkRVar ("r" ++ show i))
-    else do
-      resType <- local (\(AutToTypeState mp gr0 cache0) -> AutToTypeState { tvMap = mp
-                                                                          , graph = delEdges [(i,n) | n <- suc gr i, i `elem` dfs [n] gr] gr0
-                                                                          , cache = S.insert i cache0 }) $ do
-        let varL = TTyTVar <$> (S.toList $ fromJust $ M.lookup i tvMap)
+  -- First we check if i is in the cache.
+  -- If i is in the cache, we return a recursive variable.
+  inCache <- checkCache i
+  case inCache of
+    True -> return $ TTyRVar (MkRVar ("r" ++ show i))
+    False -> do
+      outs <- nodeToOuts i
+      gr <- asks graph
+      let (_,_,(pol,HeadCons datSet codatSet),_) = context gr i
+      let (maybeDat,maybeCodat) = (S.toList <$> datSet, S.toList <$> codatSet)
+      resType <- local (visitNode i) $ do
+        -- Creating type variables
+        varL <- nodeToTVars i
+        -- Creating data types
         datL <- case maybeDat of
           {Nothing -> return [] ;
           Just xtors -> do
             sig <- forM xtors $ \xt -> do
-              prdTypes <- forM [0..highestIndex Data xt Prd] $ \j -> do
-                typs <- sequence [nodeToType n | (EdgeSymbol Data xt' Prd j', n) <- outs, xt == xt', j == j']
-                return $ unionOrInter (applyVariance Data Prd pol) typs
-              cnsTypes <- forM [0..highestIndex Data xt Cns] $ \j -> do
-                typs <- sequence [nodeToType n | (EdgeSymbol Data xt' Cns j', n) <- outs, xt == xt', j == j']
-                return $ unionOrInter (applyVariance Data Cns pol) typs
+              let prdNodes = filterSortOuts outs Data xt Prd
+              prdTypes <- bar prdNodes Data Prd pol
+              let cnsNodes = filterSortOuts outs Data xt Cns
+              cnsTypes <- bar cnsNodes Data Cns pol
               return (MkXtorSig xt (Twice prdTypes cnsTypes))
             return [TTySimple Data sig]}
+        -- Creating codata types
         codatL <- case maybeCodat of
           {Nothing -> return [] ;
           Just xtors -> do
             sig <- forM xtors $ \xt -> do
-              prdTypes <- forM [0..highestIndex Codata xt Prd] $ \j -> do
-                typs <- sequence [nodeToType n | (EdgeSymbol Codata xt' Prd j', n) <- outs, xt == xt', j == j']
-                return $ unionOrInter (applyVariance Codata Prd pol) typs
-              cnsTypes <- forM [0..highestIndex Codata xt Cns] $ \j -> do
-                typs <- sequence [nodeToType n | (EdgeSymbol Codata xt' Cns j', n) <- outs, xt == xt', j == j']
-                return $ unionOrInter (applyVariance Codata Cns pol) typs
+              let prdNodes = filterSortOuts outs Codata xt Prd
+              prdTypes <- bar prdNodes Codata Prd pol
+              let cnsNodes = filterSortOuts outs Codata xt Cns
+              cnsTypes <- bar cnsNodes Codata Cns pol
               return (MkXtorSig xt (Twice prdTypes cnsTypes))
             return [TTySimple Codata sig]}
         return $ unionOrInter pol (varL ++ datL ++ codatL)
