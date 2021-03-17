15a506d15eab273c2ed35313a9b28899fa3dd7f2
Dennis Akdasli <dennis.akdasli@student.uni-tuebingen.de>
2021-03-17 11:25:07 +0100

    deleted comments

--- src/Translate/Translate.hs
+++ src/Translate/Translate.hs
@@ -27,34 +27,3 @@ compileArgs = compileArgsAcc (MkXtorArgs [] [])
       case compile tm of
         Left  stm -> compileArgsAcc (MkXtorArgs (stm : prds) cnss) rst
         Right stm -> compileArgsAcc (MkXtorArgs prds (stm : cnss)) rst
-
-
-
-{- FIRST TRY 
-
-
-compile :: ATerm a -> STerm pc a
-{-
-compile (BVar i) = ...
-compile (FVar n) = ...
--}
-compile (Ctor xt args')   = XtorCall PrdRep xt $ compileArgs args'
-{-
-compile (Dtor xt t args') = XtorCall CnsRep xt (...t...) seperatePrdCns $ compile <$> args'
-compile (Match t cases)   = XMatch CnsRep  ...NS... (compile t) ...
-compile (Comatch cocases) = XMatch PrdRep ... 
--}
-
-compileArgs :: [ATerm a] -> XtorArgs a
-compileArgs = compileArgsAcc (MkXtorArgs [] [])
-  where
-    compileArgsAcc xtorArgs [] = xtorArgs
-    compileArgsAcc (MkXtorArgs prds cnss) (tm:rst) =
-      case compile tm of
-        BoundVar PrdRep _   -> compileArgsAcc (MkXtorArgs (compile tm : prds) cnss) rst
-        FreeVar PrdRep _ _  -> compileArgsAcc (MkXtorArgs (compile tm : prds) cnss) rst
-        XtorCall PrdRep _ _ -> compileArgsAcc (MkXtorArgs (compile tm : prds) cnss) rst
-        XMatch PrdRep _ _   -> compileArgsAcc (MkXtorArgs (compile tm : prds) cnss) rst
-        MuAbs PrdRep _ _    -> compileArgsAcc (MkXtorArgs (compile tm : prds) cnss) rst
-        _                   -> compileArgsAcc (MkXtorArgs prds (compile tm : cnss)) rst
--}
