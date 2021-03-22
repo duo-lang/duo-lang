88dd9ea8cc679db9626fd23761975eabe738dc38
BinderDavid <binderd@informatik.uni-tuebingen.de>
2021-03-22 16:15:50 +0100

    Remove lexing part from running of parser

--- src/Parser/Definition.hs
+++ src/Parser/Definition.hs
@@ -9,13 +9,15 @@ import Control.Applicative (Alternative)
 import Control.Monad.Reader
 import Data.Set (Set)
 import qualified Data.Set as S
-import Data.Void
-import Text.Megaparsec hiding (State)
-import Text.Megaparsec.Char
-import qualified Text.Megaparsec.Char.Lexer as L
+import Data.Void (Void)
+import Text.Megaparsec
 
 import Syntax.Types
 import Utils
+
+-------------------------------------------------------------------------------------------
+-- Definition of the Parsing Monad
+-------------------------------------------------------------------------------------------
 
 data ParseReader = ParseReader
   { rvars :: Set TVar
@@ -25,26 +27,19 @@ data ParseReader = ParseReader
 defaultParseReader :: ParseReader
 defaultParseReader = ParseReader S.empty S.empty
 
--- A parser that can read values from an environment
 newtype Parser a = Parser { unParser :: ReaderT ParseReader (Parsec Void String) a }
   deriving (Functor, Applicative, Monad, MonadFail, Alternative, MonadPlus
            , MonadParsec Void String, MonadReader ParseReader)
 
-runInteractiveParser :: Parser a -> String -> Either Error a
-runInteractiveParser p input = runFileParser "<interactive>" p input
+-------------------------------------------------------------------------------------------
+-- Running a parser
+-------------------------------------------------------------------------------------------
 
 runFileParser :: FilePath -> Parser a -> String -> Either Error a
-runFileParser fp p input = case runParser (runReaderT (unParser (wrap p)) defaultParseReader) fp input of
+runFileParser fp p input = case runParser (runReaderT (unParser p) defaultParseReader) fp input of
   Left err -> Left $ ParseError (errorBundlePretty err)
   Right x -> Right x
 
+runInteractiveParser :: Parser a -> String -> Either Error a
+runInteractiveParser p input = runFileParser "<interactive>" p input
 
-wrap :: Parser a -> Parser a
-wrap p = lexeme (p <* eof)
-  where
-    -- TODO: Get rid of these here
-    sc :: Parser ()
-    sc = L.space space1 (L.skipLineComment "#") (L.skipBlockComment "###" "###")
-
-    lexeme :: Parser a -> Parser a
-    lexeme = L.lexeme sc
