eef883bbcf44d0537588aff465ee3b76d4f9cb0c
BinderDavid <binderd@informatik.uni-tuebingen.de>
2021-03-22 16:08:36 +0100

    Make parser newtype

--- src/Parser/Definition.hs
+++ src/Parser/Definition.hs
@@ -5,6 +5,7 @@ module Parser.Definition
   , runFileParser
   ) where
 
+import Control.Applicative (Alternative)
 import Control.Monad.Reader
 import Data.Set (Set)
 import qualified Data.Set as S
@@ -25,19 +26,25 @@ defaultParseReader :: ParseReader
 defaultParseReader = ParseReader S.empty S.empty
 
 -- A parser that can read values from an environment
-type Parser a = ReaderT ParseReader (Parsec Void String) a
+newtype Parser a = Parser { unParser :: ReaderT ParseReader (Parsec Void String) a }
+  deriving (Functor, Applicative, Monad, MonadFail, Alternative, MonadPlus
+           , MonadParsec Void String, MonadReader ParseReader)
 
 runInteractiveParser :: Parser a -> String -> Either Error a
 runInteractiveParser p input = runFileParser "<interactive>" p input
 
 runFileParser :: FilePath -> Parser a -> String -> Either Error a
-runFileParser fp p input = case runParser (runReaderT (lexeme (p <* eof)) defaultParseReader) fp input of
+runFileParser fp p input = case runParser (runReaderT (unParser (wrap p)) defaultParseReader) fp input of
   Left err -> Left $ ParseError (errorBundlePretty err)
   Right x -> Right x
+
+
+wrap :: Parser a -> Parser a
+wrap p = lexeme (p <* eof)
   where
     -- TODO: Get rid of these here
-    sc :: (MonadParsec Void String m) => m ()
+    sc :: Parser ()
     sc = L.space space1 (L.skipLineComment "#") (L.skipBlockComment "###" "###")
 
-    lexeme :: (MonadParsec Void String m) => m a -> m a
+    lexeme :: Parser a -> Parser a
     lexeme = L.lexeme sc
