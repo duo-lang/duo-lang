4824cb330112a0bac6a10157740febaa7bae56e6
BinderDavid <binderd@informatik.uni-tuebingen.de>
2021-03-25 17:09:50 +0100

    Modify parser to parse with free var names

--- src/Parser/ATerms.hs
+++ src/Parser/ATerms.hs
@@ -7,19 +7,19 @@ import Parser.Lexer
 import Syntax.CommonTerm
 import Syntax.ATerms
 
-fvarP :: Parser (ATerm ())
+fvarP :: Parser (ATerm FreeVarName)
 fvarP = do
   fv <- freeVarName
   return (FVar fv)
 
-ctorP :: NominalStructural -> Parser (ATerm ())
+ctorP :: NominalStructural -> Parser (ATerm FreeVarName)
 ctorP ns = do
   xt <- xtorName ns
   args <- option [] (parens $ atermP `sepBy` comma)
   return (Ctor xt args)
 
 
-dtorP :: NominalStructural -> Parser (ATerm ())
+dtorP :: NominalStructural -> Parser (ATerm FreeVarName)
 dtorP ns = do
   -- Must use atermP' here in order to avoid left-recursion in grammar!
   destructee <- atermP'
@@ -30,21 +30,21 @@ dtorP ns = do
 
 
 
-acaseP :: NominalStructural -> Parser (ACase ())
+acaseP :: NominalStructural -> Parser (ACase FreeVarName)
 acaseP ns = do
   xt <- xtorName ns
   args <- option [] (parens $ freeVarName `sepBy` comma)
   _ <- symbol "=>"
   res <- atermP
-  return (MkACase xt ((const ()) <$> args) (atermClosing args res))
+  return (MkACase xt args (atermClosing args res))
 
-acasesP :: Parser [ACase ()]
+acasesP :: Parser [ACase FreeVarName]
 acasesP = try structuralCases <|> nominalCases
   where
     structuralCases = braces $ acaseP Structural `sepBy` comma
     nominalCases = braces $ acaseP Nominal `sepBy` comma
 
-matchP :: Parser (ATerm ())
+matchP :: Parser (ATerm FreeVarName)
 matchP = do
   _ <- symbol "match"
   arg <- atermP
@@ -52,23 +52,23 @@ matchP = do
   cases <- acasesP
   return (Match arg cases)
 
-comatchP :: Parser (ATerm ())
+comatchP :: Parser (ATerm FreeVarName)
 comatchP = do
   _ <- symbol "comatch"
   cocases <- acasesP
   return (Comatch cocases)
 
-numLitP :: Parser (ATerm ())
+numLitP :: Parser (ATerm bs)
 numLitP = numToTerm <$> numP
   where
-    numToTerm :: Int -> ATerm ()
+    numToTerm :: Int -> ATerm bs
     numToTerm 0 = Ctor (MkXtorName Nominal "Zero") []
     numToTerm n = Ctor (MkXtorName Nominal "Succ") [numToTerm (n-1)]
 
 
 -- | Like atermP but without dtorP, since dtorP
 -- uses left-recursion in the grammar.
-atermP' :: Parser (ATerm ())
+atermP' :: Parser (ATerm FreeVarName)
 atermP' =
   parens atermP <|>
   numLitP <|>
@@ -78,7 +78,7 @@ atermP' =
   ctorP Nominal <|>
   fvarP
 
-atermP :: Parser (ATerm ())
+atermP :: Parser (ATerm FreeVarName)
 atermP =
   parens atermP <|>
   try (dtorP Structural) <|>
