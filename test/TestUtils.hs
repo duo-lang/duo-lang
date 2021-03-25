1375e94236e5308f6fc79d51c6d6ec1a96fdfb81
BinderDavid <binderd@informatik.uni-tuebingen.de>
2021-03-25 15:59:52 +0100

    Improve implementation of testsuite by removing error calls

--- test/TestUtils.hs
+++ test/TestUtils.hs
@@ -1,6 +1,5 @@
 module TestUtils where
 
-import qualified Data.Map as M
 import System.Directory (listDirectory)
 
 import Parser.Parser
@@ -19,25 +18,16 @@ getAvailableExamples = do
   examples <- listDirectory "examples/"
   return (("examples/" ++) <$> examples)
 
-filterEnvironment :: [String] -> Environment -> Environment
-filterEnvironment failingExamples Environment {..} =
-  Environment { prdEnv = M.filterWithKey (\k _ -> not (k `elem` failingExamples)) prdEnv
-              , cnsEnv = M.filterWithKey (\k _ -> not (k `elem` failingExamples)) cnsEnv
-              , cmdEnv = cmdEnv
-              , defEnv = defEnv
-              , declEnv = declEnv
-              }
-
 getParsedDeclarations :: FilePath -> IO (Either Error [Declaration ()])
 getParsedDeclarations fp = do
   s <- readFile fp
   return (runFileParser fp programP s)
 
-getEnvironment :: FilePath -> [String] -> IO (Either Error Environment)
-getEnvironment fp failingExamples = do
+getEnvironment :: FilePath -> IO (Either Error Environment)
+getEnvironment fp = do
   decls <- getParsedDeclarations fp
   case decls of
     Right decls -> case inferProgram decls of
-      Right env -> return $ Right (filterEnvironment failingExamples env)
-      Left (Located _ err) -> return $ Left err
-    Left err -> return $ Left err
+      Right env -> return (Right env)
+      Left (Located _ err) -> return (Left err)
+    Left err -> return (Left err)
--- test/TypeInference/StaticExamplesSpec.hs
+++ test/TypeInference/StaticExamplesSpec.hs
@@ -11,7 +11,6 @@ import Syntax.Program
 import TypeInference.InferTypes
 import TypeAutomata.ToAutomaton
 import TypeAutomata.Subsume (typeAutEqual)
-import Utils
 
 instance Show (TypeScheme pol) where
   show = ppPrint
@@ -28,33 +27,35 @@ typecheckExample env termS typS = do
 spec :: Spec
 spec = do
   describe "Typecheck specific examples" $ do
-    env <- runIO $ getEnvironment "examples/prg.ds" []
-    let env' = unsafeFromRight env
-    typecheckExample env' "\\(x)[k] => x >> k" "forall a. { 'Ap(a)[a] }"
-    typecheckExample env' "'S('Z)" "< 'S(< 'Z >) >"
-    typecheckExample env' "\\(b,x,y)[k] => b >> match { 'True => x >> k, 'False => y >> k }"
-                         "forall a. { 'Ap(< 'True | 'False >, a, a)[a] }"
-    typecheckExample env' "\\(b,x,y)[k] => b >> match { 'True => x >> k, 'False => y >> k }"
-                         "forall a b. { 'Ap(<'True|'False>, a, b)[a \\/ b] }"
-    typecheckExample env' "\\(f)[k] => (\\(x)[k] => f >> 'Ap(x)[mu*y. f >> 'Ap(y)[k]]) >> k"
-                         "forall a b. { 'Ap({ 'Ap(a \\/ b)[b] })[{ 'Ap(a)[b] }] }"
-    -- Nominal Examples
-    typecheckExample env' "\\(x)[k] => x >> match { TT => FF >> k, FF => TT >> k }"
-                         "{ 'Ap(Bool)[Bool] }"
-    typecheckExample env' "\\(x)[k] => x >> match { TT => FF >> k, FF => Zero >> k }"
-                         "{ 'Ap(Bool)[(Bool \\/ Nat)] }"
-    typecheckExample env' "\\(x)[k] => x >> match { TT => FF >> k, FF => Zero >> k }"
-                         "{ 'Ap(Bool)[(Nat \\/ Bool)] }"
-    -- addNominal
-    typecheckExample env' "comatch { 'Ap(n,m)[k] => fix >> 'Ap( comatch { 'Ap(alpha)[k] => comatch { 'Ap(m)[k] => m >> match { Zero => n >> k, Succ(p) => alpha >> 'Ap(p)[mu* w. Succ(w) >> k] }} >> k })['Ap(m)[k]] }"
-                         "forall t0. { 'Ap(t0,Nat)[(t0 \\/ Nat)] }"
-    -- mltNominal
-    typecheckExample env' "comatch { 'Ap(n,m)[k] => fix >> 'Ap(comatch { 'Ap(alpha)[k] => comatch { 'Ap(m)[k] => m >> match { Zero => Zero >> k, Succ(p) => alpha >> 'Ap(p)[mu* w. addNominal >> 'Ap(n,w)[k]] } } >> k })['Ap(m)[k]]}"
-                         "forall t0. { 'Ap((t0 /\\ Nat),Nat)[(t0 \\/ Nat)] }"
-    -- expNominal
-    typecheckExample env' "comatch { 'Ap(n,m)[k] => fix >> 'Ap(comatch { 'Ap(alpha)[k] => comatch { 'Ap(m)[k] => m >> match { Zero => Succ(Zero) >> k, Succ(p) => alpha >> 'Ap(p)[mu* w. mltNominal >> 'Ap(n,w)[k]] } } >> k })['Ap(m)[k]] }"
-                         "forall t0. { 'Ap((t0 /\\ Nat),Nat)[(t0 \\/ Nat)] }"
-    -- subSafeNominal
-    typecheckExample env' "comatch { 'Ap(n,m)[k] => fix >> 'Ap(comatch { 'Ap(alpha)[k] => comatch { 'Ap(n)[k] => comatch { 'Ap(m)[k] => m >> match { Zero => n >> k, Succ(mp) => n >> match { Zero => n >> k, Succ(np) => alpha >> 'Ap(np)['Ap(mp)[k]] }}} >> k } >> k })['Ap(n)['Ap(m)[k]]]}"
-                         "forall t0. { 'Ap((t0 /\\ Nat),Nat)[(t0 \\/ Nat)] }"
+    env <- runIO $ getEnvironment "examples/prg.ds"
+    case env of
+      Left err -> it "Could not load environment" $ expectationFailure (ppPrint err)
+      Right env' -> do
+        typecheckExample env' "\\(x)[k] => x >> k" "forall a. { 'Ap(a)[a] }"
+        typecheckExample env' "'S('Z)" "< 'S(< 'Z >) >"
+        typecheckExample env' "\\(b,x,y)[k] => b >> match { 'True => x >> k, 'False => y >> k }"
+                             "forall a. { 'Ap(< 'True | 'False >, a, a)[a] }"
+        typecheckExample env' "\\(b,x,y)[k] => b >> match { 'True => x >> k, 'False => y >> k }"
+                             "forall a b. { 'Ap(<'True|'False>, a, b)[a \\/ b] }"
+        typecheckExample env' "\\(f)[k] => (\\(x)[k] => f >> 'Ap(x)[mu*y. f >> 'Ap(y)[k]]) >> k"
+                             "forall a b. { 'Ap({ 'Ap(a \\/ b)[b] })[{ 'Ap(a)[b] }] }"
+        -- Nominal Examples
+        typecheckExample env' "\\(x)[k] => x >> match { TT => FF >> k, FF => TT >> k }"
+                             "{ 'Ap(Bool)[Bool] }"
+        typecheckExample env' "\\(x)[k] => x >> match { TT => FF >> k, FF => Zero >> k }"
+                             "{ 'Ap(Bool)[(Bool \\/ Nat)] }"
+        typecheckExample env' "\\(x)[k] => x >> match { TT => FF >> k, FF => Zero >> k }"
+                             "{ 'Ap(Bool)[(Nat \\/ Bool)] }"
+        -- addNominal
+        typecheckExample env' "comatch { 'Ap(n,m)[k] => fix >> 'Ap( comatch { 'Ap(alpha)[k] => comatch { 'Ap(m)[k] => m >> match { Zero => n >> k, Succ(p) => alpha >> 'Ap(p)[mu* w. Succ(w) >> k] }} >> k })['Ap(m)[k]] }"
+                             "forall t0. { 'Ap(t0,Nat)[(t0 \\/ Nat)] }"
+        -- mltNominal
+        typecheckExample env' "comatch { 'Ap(n,m)[k] => fix >> 'Ap(comatch { 'Ap(alpha)[k] => comatch { 'Ap(m)[k] => m >> match { Zero => Zero >> k, Succ(p) => alpha >> 'Ap(p)[mu* w. addNominal >> 'Ap(n,w)[k]] } } >> k })['Ap(m)[k]]}"
+                             "forall t0. { 'Ap((t0 /\\ Nat),Nat)[(t0 \\/ Nat)] }"
+        -- expNominal
+        typecheckExample env' "comatch { 'Ap(n,m)[k] => fix >> 'Ap(comatch { 'Ap(alpha)[k] => comatch { 'Ap(m)[k] => m >> match { Zero => Succ(Zero) >> k, Succ(p) => alpha >> 'Ap(p)[mu* w. mltNominal >> 'Ap(n,w)[k]] } } >> k })['Ap(m)[k]] }"
+                             "forall t0. { 'Ap((t0 /\\ Nat),Nat)[(t0 \\/ Nat)] }"
+        -- subSafeNominal
+        typecheckExample env' "comatch { 'Ap(n,m)[k] => fix >> 'Ap(comatch { 'Ap(alpha)[k] => comatch { 'Ap(n)[k] => comatch { 'Ap(m)[k] => m >> match { Zero => n >> k, Succ(mp) => n >> match { Zero => n >> k, Succ(np) => alpha >> 'Ap(np)['Ap(mp)[k]] }}} >> k } >> k })['Ap(n)['Ap(m)[k]]]}"
+                             "forall t0. { 'Ap((t0 /\\ Nat),Nat)[(t0 \\/ Nat)] }"
 
