23f729a781cf32b0210720ee85633786a8b935ef
BinderDavid <binderd@informatik.uni-tuebingen.de>
2021-04-08 17:05:07 +0200

    Add comments to constraint solver

--- src/TypeInference/SolveConstraints.hs
+++ src/TypeInference/SolveConstraints.hs
@@ -115,41 +115,105 @@ checkXtor xtors2 (MkXtorSig xtName (MkTypArgs prd1 cns1)) = do
   MkXtorSig _ (MkTypArgs prd2 cns2) <- lookupXtor xtName xtors2
   pure $ zipWith (SubType ()) prd1 prd2 ++ zipWith (SubType ()) cns2 cns1
 
+-- | The `subConstraints` function takes a complex constraint, and decomposes it
+-- into simpler constraints. A constraint is complex if it is not atomic. An atomic
+-- constraint has a type variable on the right or left hand side of the constraint.
+--
+-- The `subConstraints` function is the function which will produce the error if the
+-- constraint set generated from a program is not solvable.
 subConstraints :: Constraint () -> SolverM [Constraint ()]
--- Set constraints
-subConstraints (SubType _ (TySet PosRep tys) ty)  = return [SubType () ty' ty | ty' <- tys]
-subConstraints (SubType _ ty (TySet NegRep tys))  = return [SubType () ty ty' | ty' <- tys]
--- Recursive constraints
-subConstraints (SubType _ ty@(TyRec _ _ _) ty')  = return [SubType () (unfoldRecType ty) ty']
-subConstraints (SubType _ ty' ty@(TyRec _ _ _))  = return [SubType () ty' (unfoldRecType ty)]
--- Data/Data and Codata/Codata constraints
-subConstraints (SubType _ (TyData PosRep xtors1) (TyData NegRep xtors2)) = do
-  constraints <- forM xtors1 (checkXtor xtors2)
+-- Intersection and union constraints:
+--
+-- If the left hand side of the constraint is a intersection type, or the
+-- right hand side is a union type, then the constraint can be directly decomposed
+-- into structurally smaller subconstraints. E.g.:
+--
+--     ty1 /\ ty2 <: ty3         ~>     ty1 <: ty3   AND  ty2 <: ty3
+--     ty1 <: ty2 \/ ty3         ~>     ty1 <: ty2   AND  ty1 <: ty3
+--
+subConstraints (SubType _ (TySet PosRep tys) ty) =
+  return [SubType () ty' ty | ty' <- tys]
+subConstraints (SubType _ ty (TySet NegRep tys)) =
+  return [SubType () ty ty' | ty' <- tys]
+-- Recursive constraints:
+--
+-- If the left hand side or the right hand side of the constraint is a recursive
+-- mu-type, the mu-type gets unrolled once. Note that this case makes it non-obvious
+-- that constraint generation is going to terminate. Examples:
+--
+--     rec a.ty1 <: ty2          ~>     ty1 [rec a.ty1 / a] <: ty2
+--     ty1 <: rec a.ty2          ~>     ty1 <: ty2 [rec a.ty2 / a]
+--
+subConstraints (SubType _ ty@(TyRec _ _ _) ty') =
+  return [SubType () (unfoldRecType ty) ty']
+subConstraints (SubType _ ty' ty@(TyRec _ _ _)) =
+  return [SubType () ty' (unfoldRecType ty)]
+-- Constraints between structural data or codata types.
+--
+-- Constraints between structural data and codata types generates constraints based
+-- on the xtors of the two types. In order to generate the constraints, the helper
+-- function `checkXtors` is invoked.
+--
+--     < ctors1 > <: < ctors2 >  ~>     [ checkXtors ctors2 ctor | ctor <- ctors1 ]
+--     { dtors1 } <: { dtors2 }  ~>     [ checkXtors dtors1 dtor | dtor <- dtors2 ]
+--
+subConstraints (SubType _ (TyData PosRep ctors1) (TyData NegRep ctors2)) = do
+  constraints <- forM ctors1 (checkXtor ctors2)
   pure $ concat constraints
-subConstraints (SubType _ (TyCodata PosRep xtors1) (TyCodata NegRep xtors2)) = do
-  constraints <- forM xtors2 (checkXtor xtors1)
+subConstraints (SubType _ (TyCodata PosRep dtors1) (TyCodata NegRep dtors2)) = do
+  constraints <- forM dtors2 (checkXtor dtors1)
   pure $ concat constraints
--- Nominal/Nominal Constraint
-subConstraints (SubType _ (TyNominal _ tn1) (TyNominal _ tn2)) | tn1 == tn2 = return []
-                                                               | otherwise = throwSolverError ["The following nominal types are incompatible:"
-                                                                                              , "    " ++ ppPrint tn1
-                                                                                              , "and"
-                                                                                              , "    " ++ ppPrint tn2 ]
--- Data/Codata and Codata/Data Constraints
-subConstraints cs@(SubType _ (TyData _ _) (TyCodata _ _))
-  = throwSolverError [ "Constraint:"
-                     , "     " ++ ppPrint cs
-                     , "is unsolvable. A data type can't be a subtype of a codata type!" ]
-subConstraints cs@(SubType _ (TyCodata _ _) (TyData _ _))
-  = throwSolverError [ "Constraint:"
-                     , "     "++ ppPrint cs
-                     , "is unsolvable. A codata type can't be a subtype of a data type!" ]
--- Nominal/XData and XData/Nominal Constraints
-subConstraints (SubType _ (TyData _ _) (TyNominal _ _)) = throwSolverError ["Cannot constrain nominal by structural type"]
-subConstraints (SubType _ (TyCodata _ _) (TyNominal _ _)) = throwSolverError ["Cannot constrain nominal by structural type"]
-subConstraints (SubType _ (TyNominal _ _) (TyData _ _)) = throwSolverError ["Cannot constrain nominal by structural type"]
-subConstraints (SubType _ (TyNominal _ _) (TyCodata _ _)) = throwSolverError ["Cannot constrain nominal by structural type"]
--- subConstraints should never be called if the upper or lower bound is a unification variable.
+-- Constraints between nominal types:
+--
+-- We currently do not have any subtyping relationshipts between nominal types.
+-- We therefore only have to check if the two nominal types are identical. E.g.:
+--
+--     Bool <: Nat               ~>     FAIL
+--     Bool <: Bool              ~>     []
+--
+subConstraints (SubType _ (TyNominal _ tn1) (TyNominal _ tn2)) =
+  case tn1 == tn2 of
+    True  -> pure []
+    False -> throwSolverError ["The following nominal types are incompatible:"
+                              , "    " ++ ppPrint tn1
+                              , "and"
+                              , "    " ++ ppPrint tn2 ]
+-- Constraints between structural data and codata types:
+--
+-- A constraint between a structural data type and a structural codata type
+-- cannot be solved. E.g.:
+--
+--     < ctors > <: { dtors }    ~>     FAIL
+--     { dtors } <: < ctors >    ~>     FAIL
+--
+subConstraints cs@(SubType _ (TyData _ _) (TyCodata _ _)) =
+  throwSolverError [ "Constraint:"
+                   , "     " ++ ppPrint cs
+                   , "is unsolvable. A data type can't be a subtype of a codata type!" ]
+subConstraints cs@(SubType _ (TyCodata _ _) (TyData _ _)) =
+  throwSolverError [ "Constraint:"
+                   , "     " ++ ppPrint cs
+                   , "is unsolvable. A codata type can't be a subtype of a data type!" ]
+-- Constraints between nominal and a structural types:
+--
+-- These constraints are not solvable. E.g.:
+--
+--     < ctors > <: Nat          ~>     FAIL
+--
+subConstraints (SubType _ (TyData _ _) (TyNominal _ _)) =
+  throwSolverError ["Cannot constrain nominal by structural type"]
+subConstraints (SubType _ (TyCodata _ _) (TyNominal _ _)) =
+  throwSolverError ["Cannot constrain nominal by structural type"]
+subConstraints (SubType _ (TyNominal _ _) (TyData _ _)) =
+  throwSolverError ["Cannot constrain nominal by structural type"]
+subConstraints (SubType _ (TyNominal _ _) (TyCodata _ _)) =
+  throwSolverError ["Cannot constrain nominal by structural type"]
+-- Atomic constraints:
+--
+-- Atomic constraints, i.e. constraints between a type and a type variable, should be
+-- dealt with in the function `solve`. Calling the function `subConstraints` with an
+-- atomic constraint is an implementation bug.
+--
 subConstraints (SubType _ ty1@(TyVar _ _) ty2) =
   throwSolverError ["subConstraints should only be called if neither upper nor lower bound are unification variables"
                    , ppPrint ty1
