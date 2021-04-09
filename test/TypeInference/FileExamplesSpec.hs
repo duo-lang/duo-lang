9b7e01e4adfe4004ca07855c4cd07e32c3d2a5e9
BinderDavid <dbinder1989@googlemail.com>
2021-04-09 20:55:20 +0200

    Remove redundant tests

--- test/TypeInference/FileExamplesSpec.hs
+++ test/TypeInference/FileExamplesSpec.hs
@@ -3,28 +3,9 @@ module TypeInference.FileExamplesSpec ( spec ) where
 import           Test.Hspec
 import           Control.Monad (forM_)
 
-import qualified Data.Map as M
-import Data.Either (isRight)
-
 import TestUtils
 import Pretty.Pretty
 import Pretty.Errors ()
-import Syntax.STerms
-import Syntax.Types
-import Syntax.Program
-import TypeInference.InferTypes
-import Utils
-
-instance Show (TypeScheme pol) where
-  show = ppPrint
-
-checkTerm :: PrdCnsRep pc -> Environment FreeVarName -> (FreeVarName, STerm pc Loc FreeVarName) -> SpecWith ()
-checkTerm rep env (name,term) = it (name ++ " can be typechecked correctly") $
-  inferSTerm rep term env `shouldSatisfy` isRight
-
-checkCommand :: Environment FreeVarName -> (FreeVarName, Command Loc FreeVarName) -> SpecWith ()
-checkCommand env (name,cmd) = it (name ++ " can be typechecked") $
-  checkCmd cmd env `shouldSatisfy` isRight
 
 -- | Typecheck the programs in the toplevel "examples/" subfolder.
 spec :: Spec
@@ -36,4 +17,4 @@ spec = do
         env <- runIO $ getEnvironment example
         case env of
           Left err -> it "Could not load examples" $ expectationFailure (ppPrint err)
-          Right env -> return ()
+          Right _env -> return ()
