76738c1d2d49ffc73b7806d8046ace9059429d77
Dennis Akdasli <dennis.akdasli@student.uni-tuebingen.de>
2021-04-20 22:50:56 +0200

    isSubst and docu
    - implement 'substituition predicate'
    - extend documentation

--- src/Eval/STerms.hs
+++ src/Eval/STerms.hs
@@ -26,7 +26,7 @@ lookupCase xt cases = case find (\MkSCase { scase_name } -> xt == scase_name) ca
 lengthXtorArgs :: XtorArgs () FreeVarName -> Twice Int
 lengthXtorArgs MkXtorArgs { prdArgs, cnsArgs } = Twice (length prdArgs) (length cnsArgs)
 
-checkArgs :: PrettyAnn FreeVarName => Command () FreeVarName -> Twice [FreeVarName] -> XtorArgs () FreeVarName -> EvalM FreeVarName ()
+checkArgs :: Command () FreeVarName -> Twice [FreeVarName] -> XtorArgs () FreeVarName -> EvalM FreeVarName ()
 checkArgs cmd argTypes args =
   if fmap length argTypes == lengthXtorArgs args
   then return ()
@@ -34,12 +34,12 @@ checkArgs cmd argTypes args =
                         "\"\nArgument lengths don't coincide.")
 
 -- | Returns Notihng if command was in normal form, Just cmd' if cmd reduces to cmd' in one step
-evalSTermOnce :: PrettyAnn FreeVarName => Command () FreeVarName -> EvalM FreeVarName (Maybe (Command () FreeVarName))
+evalSTermOnce :: Command () FreeVarName -> EvalM FreeVarName (Maybe (Command () FreeVarName))
 evalSTermOnce (Done _) = return Nothing
 evalSTermOnce (Print _ _) = return Nothing
 evalSTermOnce (Apply _ prd cns) = evalApplyOnce prd cns
 
-evalApplyOnce :: PrettyAnn FreeVarName => STerm Prd () FreeVarName -> STerm Cns () FreeVarName -> EvalM FreeVarName (Maybe (Command () FreeVarName))
+evalApplyOnce :: STerm Prd () FreeVarName -> STerm Cns () FreeVarName -> EvalM FreeVarName (Maybe (Command () FreeVarName))
 -- Free variables have to be looked up in the environment.
 evalApplyOnce (FreeVar _ PrdRep fv) cns = do
   (prd,_) <- lookupPrd fv
@@ -48,36 +48,45 @@ evalApplyOnce prd (FreeVar _ CnsRep fv) = do
   (cns,_) <- lookupCns fv
   return (Just (Apply () prd cns))
 -- (Co-)Pattern matches are evaluated using the ordinary pattern matching rules.
-evalApplyOnce prd@(XtorCall _ PrdRep _ _) cns@(XMatch _ CnsRep _ _) = do
-  order <- lookupEvalOrder
-  evalMatchOnce prd cns order
+-- Pattern match depend on wether all arguments can be subst. into the Pattern.
+evalApplyOnce prd@(XtorCall _ PrdRep _ args) cns@(XMatch _ CnsRep _ _) = do
+  areSubst <- areAllSubst args
+  case areSubst of
+    True  -> substArgs prd cns
+    False -> focusingStep prd cns
   where
-    evalMatchOnce :: PrettyAnn FreeVarName => STerm Prd () FreeVarName -> STerm Cns () FreeVarName -> EvalOrder -> EvalM FreeVarName (Maybe (Command () FreeVarName))
-    --evalMatchOnce :: PrettyAnn FreeVarName => STerm Prd () FreeVarName -> STerm Cns () FreeVarName -> EvalOrder -> EvalM FreeVarName (Maybe (Command () FreeVarName))
-    evalMatchOnce prd@(XtorCall _ PrdRep xt args) cns@(XMatch _ CnsRep _ cases) CBN = do
+    -- Subst all arguments of Ctor into corresponding pattern.
+    substArgs :: STerm Prd () FreeVarName -> STerm Cns () FreeVarName -> EvalM FreeVarName (Maybe (Command () FreeVarName))
+    substArgs prd@(XtorCall _ PrdRep xt args) cns@(XMatch _ CnsRep _ cases) = do
       (MkSCase _ argTypes cmd') <- lookupCase xt cases
       checkArgs (Apply () prd cns) argTypes args
-      return (Just (commandOpening args cmd')) --reduction is just opening
+      return (Just (commandOpening args cmd')) -- reduction is just opening
+    substArgs _ _ = error "unrechable cases due to local definition of substArgs"
+    -- Focus on first non-subst. argument and evaluate this from here onwards.
+    focusingStep :: STerm Prd () FreeVarName -> STerm Cns () FreeVarName -> EvalM FreeVarName (Maybe (Command () FreeVarName))
+    focusingStep (XtorCall ext PrdRep xt args) cns = 
+      return $ Just $ Apply ext (getMu args) (MuAbs ext CnsRep "r" (Apply ext (XtorCall ext PrdRep xt (replaceMu args)) cns))
+    focusingStep _ _ = error "unrechable cases due to local definition of focusingStep"
 
-    evalMatchOnce prd@(XtorCall ext PrdRep xt args) cns CBV | noMu args = evalMatchOnce prd cns CBN
-                                                            | otherwise = -- focusing step                                 ???
-                                                                 return (Just (Apply ext (getMuAbs args) (MuAbs ext CnsRep "r" (Apply ext (XtorCall ext PrdRep xt (replaceMu args)) cns))))
-    evalMatchOnce _ _ _ = error "unreachable error"
-
-evalApplyOnce prd@(XMatch _ PrdRep _ _) cns@(XtorCall _ CnsRep _ _) = do
-  order <- lookupEvalOrder
-  evalComatchOnce prd cns order
+-- Copattern matches.
+evalApplyOnce prd@(XMatch _ PrdRep _ _) cns@(XtorCall _ CnsRep _ args) = do
+  areSubst <- areAllSubst args
+  case areSubst of
+    True  -> substArgs prd cns
+    False -> focusingStep prd cns
   where
-    evalComatchOnce :: PrettyAnn FreeVarName => STerm Prd () FreeVarName -> STerm Cns () FreeVarName -> EvalOrder -> EvalM FreeVarName (Maybe (Command () FreeVarName))
-    evalComatchOnce prd@(XMatch _ PrdRep _ cases) cns@(XtorCall _ CnsRep xt args) CBN = do
+    -- Subst all arguments of Dtor into corresponding pattern.
+    substArgs :: STerm Prd () FreeVarName -> STerm Cns () FreeVarName -> EvalM FreeVarName (Maybe (Command () FreeVarName))
+    substArgs prd@(XMatch _ PrdRep _ cases) cns@(XtorCall _ CnsRep xt args) = do
       (MkSCase _ argTypes cmd') <- lookupCase xt cases
       checkArgs (Apply () prd cns) argTypes args
-      return (Just (commandOpening args cmd')) --reduction is just opening
-
-    evalComatchOnce prd cns@(XtorCall ext CnsRep xt args) CBV | noMu args = evalComatchOnce prd cns CBN
-                                                              | otherwise = -- focusing step                                   ???
-                                                                  return $ Just $ Apply ext (getMuAbs args) $ MuAbs ext CnsRep "r" $ Apply ext prd (XtorCall ext CnsRep xt (replaceMu args))
-    evalComatchOnce _ _ _ = error "unreachable error"
+      return (Just (commandOpening args cmd')) -- reduction is just opening
+    substArgs _ _ = error "unrechable cases due to local definition of substArgs"
+    -- Focus on first non-subst. argument and evaluate this from here onwards.
+    focusingStep :: STerm Prd () FreeVarName -> STerm Cns () FreeVarName -> EvalM FreeVarName (Maybe (Command () FreeVarName))
+    focusingStep prd (XtorCall ext CnsRep xt args) =
+      return $ Just $ Apply ext (getMu args) (MuAbs ext CnsRep "r" $ Apply ext prd (XtorCall ext CnsRep xt (replaceMu args)))
+    focusingStep _ _ = error "unrechable cases due to local definition of focusingStep"
 
 -- Mu abstractions have to be evaluated while taking care of evaluation order.
 evalApplyOnce prd@(MuAbs _ PrdRep _ cmd) cns@(MuAbs _ CnsRep _ cmd') = do
@@ -95,7 +104,7 @@ evalApplyOnce (XMatch _ _ _ _) (XMatch _ _ _ _) = throwEvalError "Cannot evaluat
 evalApplyOnce (XtorCall _ _ _ _) (XtorCall _ _ _ _) = throwEvalError "Cannot evaluate constructor applied to destructor"
 
 -- | Return just thef final evaluation result
-eval :: PrettyAnn FreeVarName => Command () FreeVarName -> EvalM FreeVarName (Command () FreeVarName)
+eval :: Command () FreeVarName -> EvalM FreeVarName (Command () FreeVarName)
 eval cmd = do
   cmd' <- evalSTermOnce cmd
   case cmd' of
@@ -103,10 +112,10 @@ eval cmd = do
     Just cmd' -> eval cmd'
 
 -- | Return all intermediate evaluation results
-evalSteps :: PrettyAnn FreeVarName => Command () FreeVarName -> EvalM FreeVarName [Command () FreeVarName]
+evalSteps :: Command () FreeVarName -> EvalM FreeVarName [Command () FreeVarName]
 evalSteps cmd = evalSteps' [cmd] cmd
   where
-    evalSteps' :: PrettyAnn FreeVarName => [Command () FreeVarName] -> Command () FreeVarName -> EvalM FreeVarName [Command () FreeVarName]
+    evalSteps' :: [Command () FreeVarName] -> Command () FreeVarName -> EvalM FreeVarName [Command () FreeVarName]
     evalSteps' cmds cmd = do
       cmd' <- evalSTermOnce cmd
       case cmd' of
@@ -114,24 +123,31 @@ evalSteps cmd = evalSteps' [cmd] cmd
         Just cmd' -> evalSteps' (cmds ++ [cmd']) cmd'
 
 
--- | helper functions for CBV evaluation of match and comatch
+-- | Helper functions for CBV evaluation of match and comatch
 
--- | replace currently evaluated argument in Xtor
+-- | Replace currently evaluated MuAbs-argument in Xtor with bound variable
 replaceMu :: XtorArgs ext FreeVarName -> XtorArgs ext FreeVarName
 replaceMu MkXtorArgs { prdArgs, cnsArgs } = MkXtorArgs (replaceMuPrd prdArgs) cnsArgs
---  where
-replaceMuPrd :: [STerm pc ext FreeVarName] -> [STerm pc ext FreeVarName] 
-replaceMuPrd (MuAbs ext PrdRep _ _ : prdArgs) = BoundVar ext PrdRep (0,0) : prdArgs
-replaceMuPrd (prd                  : prdArgs) = prd : replaceMuPrd prdArgs
-replaceMuPrd []                               = []
+  where
+    replaceMuPrd :: [STerm pc ext FreeVarName] -> [STerm pc ext FreeVarName] 
+    replaceMuPrd (MuAbs ext PrdRep _ _ : prdArgs) = BoundVar ext PrdRep (0,0) : prdArgs
+    replaceMuPrd (prd                  : prdArgs) = prd : replaceMuPrd prdArgs
+    replaceMuPrd []                               = []
 
--- | gets MuAbs-argrument from Xtor
-getMuAbs :: XtorArgs ext FreeVarName -> STerm Prd ext FreeVarName
-getMuAbs MkXtorArgs { prdArgs } = head $ filter isMu prdArgs
+-- | Gets the first MuAbs-argrument from Xtor
+getMu :: XtorArgs ext FreeVarName -> STerm Prd ext FreeVarName
+getMu MkXtorArgs { prdArgs } = head $ filter isMu prdArgs
 
--- | checks if no arguments is a MuAbs
-noMu :: XtorArgs ext FreeVarName -> Bool
-noMu MkXtorArgs { prdArgs } = all (not . isMu) prdArgs
+-- | Checks wether all producer arguments are substitutable.
+-- | The evaluation order determines which arguments are substitutable.
+areAllSubst :: XtorArgs ext FreeVarName -> EvalM FreeVarName Bool
+areAllSubst MkXtorArgs { prdArgs } = do
+  order <- lookupEvalOrder
+  return $ all (isSubst order) prdArgs
+  where
+    isSubst :: EvalOrder -> STerm pc ext FreeVarName -> Bool
+    isSubst CBV t = not $ isMu t -- subst every argument, unequal to Mu abstractions
+    isSubst CBN _ = True         -- subst every argument
 
 isMu :: STerm pc ext FreeVarName -> Bool
 isMu (MuAbs _ PrdRep _ _) = True
