2a7246e2b2689d2fe992029a002ac073319c71de
BinderDavid <binderd@informatik.uni-tuebingen.de>
2021-04-26 17:36:37 +0200

    Move FlowAnalysis to FromAutomaton

--- src/TypeAutomata/FromAutomaton.hs
+++ src/TypeAutomata/FromAutomaton.hs
@@ -4,11 +4,14 @@ import Syntax.CommonTerm
 import Syntax.Types
 import Syntax.TypeAutomaton
 import Utils
-import TypeAutomata.FlowAnalysis
 
+import Control.Monad.State
 import Control.Monad.Reader
 import Data.Maybe (fromJust)
 
+import Data.List (intersect, maximumBy)
+import Data.Ord (comparing)
+import Data.Graph.Inductive.PatriciaTree
 import Data.Functor.Identity
 import Data.Set (Set)
 import qualified Data.Set as S
@@ -17,6 +20,49 @@ import qualified Data.Map as M
 
 import Data.Graph.Inductive.Graph
 import Data.Graph.Inductive.Query.DFS (dfs)
+
+-------------------------------------------------------------------------------------
+-- Flow analysis
+-------------------------------------------------------------------------------------
+
+type FlowGraph = Gr () ()
+
+-- | Generate a graph consisting only of the flow_edges of the type automaton.
+genFlowGraph :: TypeAut' EdgeLabelNormal f pol -> FlowGraph
+genFlowGraph TypeAut{..} = mkGraph [(n,()) | n <- nodes ta_gr] [(i,j,()) | (i,j) <- ta_flowEdges]
+
+flowComponent :: FlowGraph -> Node -> [Node]
+flowComponent flgr i =
+  let
+    ns = neighbors flgr i
+  in
+    if null ns
+      then [i]
+      else ns ++ (foldr1 intersect) (map (neighbors flgr) ns)
+
+freshTVar :: State Int TVar
+freshTVar = do
+  n <- get
+  modify (+1)
+  return (MkTVar ("t" ++ show n))
+
+flowAnalysisState :: FlowGraph -> State Int (Map Node (Set TVar))
+flowAnalysisState flgr =
+    let
+      nextNode = maximumBy (comparing (length . flowComponent flgr)) (nodes flgr)
+      comp = flowComponent flgr nextNode
+      newGr = delEdges [(x,y) | (x,y) <- edges flgr, x `elem` comp, y `elem` comp] flgr
+    in
+      if length comp < 2
+        then return (M.fromList [(n,S.empty) | n <- nodes flgr])
+        else do
+          tv <- freshTVar
+          rest <- flowAnalysisState newGr
+          return $ foldr (.) id (map (M.adjust (S.insert tv)) comp) rest
+
+getFlowAnalysisMap :: TypeAut' EdgeLabelNormal f pol -> Map Node (Set TVar)
+getFlowAnalysisMap aut = fst $ runState (flowAnalysisState (genFlowGraph aut)) 0
+
 
 --------------------------------------------------------------------------
 -- Type automata -> Types
