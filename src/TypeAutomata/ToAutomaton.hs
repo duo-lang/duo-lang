9db28db0496970d503ad284c409624e360148496
BinderDavid <binderd@informatik.uni-tuebingen.de>
2021-04-27 17:36:23 +0200

    Simplify ToAutomaton

--- src/TypeAutomata/ToAutomaton.hs
+++ src/TypeAutomata/ToAutomaton.hs
@@ -20,38 +20,86 @@ import Data.Graph.Inductive.Graph (Node)
 import qualified Data.Graph.Inductive.Graph as G
 
 --------------------------------------------------------------------------
--- The Monad
+-- The TypeToAutomaton (TTA) Monad
 --------------------------------------------------------------------------
 
+-- | The lookup environment allows to map TVars to Nodes:
+--
+-- Recursive type variables have a polarity. They are therefore mapped to
+-- either `(Just n, Nothing)` or `(Nothing, Just n)`
+--
+-- Unification variables exist both positively and negatively. They are therefore
+-- mapped to a pair `(Just n, Just m)`
 data LookupEnv = LookupEnv { tvarEnv :: Map TVar (Maybe Node, Maybe Node) }
 
-type TypeToAutM a = StateT (TypeAutCore EdgeLabelEpsilon) (ReaderT LookupEnv (Except Error)) a
+type TTA a = StateT (TypeAutCore EdgeLabelEpsilon) (ReaderT LookupEnv (Except Error)) a
 
-runTypeAut :: TypeAutCore EdgeLabelEpsilon -> LookupEnv -> TypeToAutM a -> Either Error (a, TypeAutCore EdgeLabelEpsilon)
+runTypeAut :: TypeAutCore EdgeLabelEpsilon
+           -- ^ The initial TypeAutomaton to start the computation.
+           -> LookupEnv
+           -- ^ The initial lookup environment.
+           -> TTA a
+           -- ^ The computation to run.
+           -> Either Error (a, TypeAutCore EdgeLabelEpsilon)
 runTypeAut graph lookupEnv f = runExcept (runReaderT (runStateT f graph) lookupEnv)
+
+
+-- | Every type variable is mapped to a pair of nodes.
+createNodes :: [TVar] -> [(TVar, (Node, NodeLabel), (Node, NodeLabel), FlowEdge)]
+createNodes tvars = createNode <$> (createPairs tvars)
+  where
+    createNode :: (TVar, Node, Node) -> (TVar, (Node, NodeLabel), (Node, NodeLabel), FlowEdge)
+    createNode (tv, posNode, negNode) = (tv, (posNode, emptyNodeLabel Pos), (negNode, emptyNodeLabel Neg), (posNode, negNode))
+
+    createPairs :: [TVar] -> [(TVar,Node,Node)]
+    createPairs tvs = (\i -> (tvs !! i, 2 * i, 2 * i + 1)) <$> [0..length tvs - 1]
+
+
+initialize :: [TVar] -> (TypeAutCore EdgeLabelEpsilon, LookupEnv)
+initialize tvars =
+  let
+    nodes = createNodes tvars
+    initAut = TypeAutCore
+              { ta_gr = G.mkGraph ([pos | (_,pos,_,_) <- nodes] ++ [neg | (_,_,neg,_) <- nodes]) []
+              , ta_flowEdges = [ flowEdge | (_,_,_,flowEdge) <- nodes]
+              }
+    lookupEnv = LookupEnv { tvarEnv = M.fromList [(tv, (Just posNode,Just negNode)) | (tv,(posNode,_),(negNode,_),_) <- nodes]
+                          }
+  in
+    (initAut, lookupEnv)
+
+-- | An alternative to `runTypeAut` where the initial state is constructed from a list of Tvars.
+runTypeAutTvars :: [TVar]
+                -> TTA a
+                -> Either Error (a, TypeAutCore EdgeLabelEpsilon)
+runTypeAutTvars tvars m = do
+  let (aut, env) = initialize tvars
+  runTypeAut aut env m
 
 --------------------------------------------------------------------------
 -- Helper functions
 --------------------------------------------------------------------------
 
-throwAutomatonError :: [String] -> TypeToAutM a
+throwAutomatonError :: [String] -> TTA a
 throwAutomatonError msg = throwError $ TypeAutomatonError (unlines msg)
 
-modifyGraph :: (TypeGrEps -> TypeGrEps) -> TypeToAutM ()
-modifyGraph f = modify (\(aut@TypeAutCore { ta_gr }) -> aut { ta_gr = f ta_gr })
+modifyGraph :: (TypeGrEps -> TypeGrEps) -> TTA ()
+modifyGraph f = modify go
+  where
+    go aut@TypeAutCore { ta_gr } = aut { ta_gr = f ta_gr }
 
-insertNode :: Node -> NodeLabel -> TypeToAutM ()
+insertNode :: Node -> NodeLabel -> TTA ()
 insertNode node nodelabel = modifyGraph (G.insNode (node, nodelabel))
 
-insertEdges :: [(Node,Node,EdgeLabelEpsilon)] -> TypeToAutM ()
+insertEdges :: [(Node,Node,EdgeLabelEpsilon)] -> TTA ()
 insertEdges edges = modifyGraph (G.insEdges edges)
 
-newNodeM :: TypeToAutM Node
+newNodeM :: TTA Node
 newNodeM = do
   graph <- gets ta_gr
   pure $ (head . G.newNodes 1) graph
 
-lookupTVar :: PolarityRep pol -> TVar -> TypeToAutM Node
+lookupTVar :: PolarityRep pol -> TVar -> TTA Node
 lookupTVar PosRep tv = do
   tvarEnv <- asks tvarEnv
   case M.lookup tv tvarEnv of
@@ -93,7 +141,7 @@ lookupTVar NegRep tv = do
 sigToLabel :: XtorSig pol -> XtorLabel
 sigToLabel (MkXtorSig name (MkTypArgs prds cnss)) = MkXtorLabel name (length prds) (length cnss)
 
-insertXtors :: DataCodata -> Polarity -> [XtorSig pol] -> TypeToAutM Node
+insertXtors :: DataCodata -> Polarity -> [XtorSig pol] -> TTA Node
 insertXtors dc pol xtors = do
   newNode <- newNodeM
   insertNode newNode (singleNodeLabel pol dc (S.fromList (sigToLabel <$> xtors)))
@@ -106,7 +154,7 @@ insertXtors dc pol xtors = do
       insertEdges [(newNode, cnsNode, EdgeSymbol dc xt Cns j)]
   return newNode
 
-insertType :: Typ pol -> TypeToAutM Node
+insertType :: Typ pol -> TTA Node
 insertType (TyVar rep tv) = lookupTVar rep tv
 insertType (TySet rep tys) = do
   newNode <- newNodeM
@@ -133,39 +181,14 @@ insertType (TyNominal rep tn) = do
 
 
 --------------------------------------------------------------------------
--- 
+--
 --------------------------------------------------------------------------
-
--- | Every type variable is mapped to a pair of nodes.
-createNodes :: [TVar] -> [(TVar, (Node, NodeLabel), (Node, NodeLabel), FlowEdge)]
-createNodes tvars = createNode <$> (createPairs tvars)
-  where
-    createNode :: (TVar, Node, Node) -> (TVar, (Node, NodeLabel), (Node, NodeLabel), FlowEdge)
-    createNode (tv, posNode, negNode) = (tv, (posNode, emptyNodeLabel Pos), (negNode, emptyNodeLabel Neg), (posNode, negNode))
-
-    createPairs :: [TVar] -> [(TVar,Node,Node)]
-    createPairs tvs = (\i -> (tvs !! i, 2 * i, 2 * i + 1)) <$> [0..length tvs - 1]
-
-
-createInitialFromTypeScheme :: [TVar] -> (TypeAutCore EdgeLabelEpsilon, LookupEnv)
-createInitialFromTypeScheme tvars =
-  let
-    nodes = createNodes tvars
-    initAut = TypeAutCore
-              { ta_gr = G.mkGraph ([pos | (_,pos,_,_) <- nodes] ++ [neg | (_,_,neg,_) <- nodes]) []
-              , ta_flowEdges = [ flowEdge | (_,_,_,flowEdge) <- nodes]
-              }
-    lookupEnv = LookupEnv { tvarEnv = M.fromList [(tv, (Just posNode,Just negNode)) | (tv,(posNode,_),(negNode,_),_) <- nodes]
-                          }
-  in
-    (initAut, lookupEnv)
 
 
 -- turns a type into a type automaton with prescribed start polarity.
 typeToAut :: TypeScheme pol -> Either Error (TypeAutDet pol)
 typeToAut (TypeScheme tvars ty) = do
-  let (initAut, lookupEnv) = createInitialFromTypeScheme tvars
-  (start, aut) <- runTypeAut initAut lookupEnv (insertType ty)
+  (start, aut) <- runTypeAutTvars tvars (insertType ty)
   let newaut = TypeAut { ta_pol = getPolarity ty
                        , ta_starts = [start]
                        , ta_core = aut
@@ -173,7 +196,7 @@ typeToAut (TypeScheme tvars ty) = do
   pure $ (minimize . removeAdmissableFlowEdges . determinize . removeEpsilonEdges) newaut
 
 -- | Turns the output of the constraint solver into an automaton by using epsilon-edges to represent lower and upper bounds
-insertEpsilonEdges :: SolverResult -> TypeToAutM ()
+insertEpsilonEdges :: SolverResult -> TTA ()
 insertEpsilonEdges solverResult =
   forM_ (M.toList solverResult) $ \(tv, vstate) -> do
     i <- lookupTVar PosRep tv
@@ -187,8 +210,6 @@ insertEpsilonEdges solverResult =
 
 solverStateToTypeAut :: SolverResult -> PolarityRep pol -> Typ pol -> Either Error (TypeAut pol)
 solverStateToTypeAut solverResult pol ty = do
-  let (initAut, lookupEnv) = createInitialFromTypeScheme (M.keys solverResult)
-  ((),aut0) <- runTypeAut initAut lookupEnv (insertEpsilonEdges solverResult)
-  (start, aut1) <- runTypeAut aut0 lookupEnv (insertType ty)
-  let newAut = TypeAut { ta_starts = [start], ta_pol = pol, ta_core = aut1 }
+  (start,aut) <- runTypeAutTvars (M.keys solverResult) $ insertEpsilonEdges solverResult >> insertType ty
+  let newAut = TypeAut { ta_starts = [start], ta_pol = pol, ta_core = aut }
   return $ (removeIslands . removeEpsilonEdges) newAut
