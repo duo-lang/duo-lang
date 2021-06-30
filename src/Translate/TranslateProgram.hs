6c8cc77d4cae95836ceec27fc6cc8630ab9f45d4
Dennis Akdasli <dennis.akdasli@student.uni-tuebingen.de>
2021-06-30 14:07:35 +0200

    typescheme inserted

--- src/Translate/TranslateProgram.hs
+++ src/Translate/TranslateProgram.hs
@@ -10,5 +10,5 @@ compileProgram :: Program a b -> Program () ()
 compileProgram ps = compileDecl <$> ps
   where
     compileDecl :: Declaration a b -> Declaration () ()
-    compileDecl (DefDecl isRec _ v t) = PrdDecl isRec () v $ compile t
+    compileDecl (DefDecl isRec _ v ts t) = PrdDecl isRec () v ts $ compile t
     compileDecl decl = bimap (const ()) (const ()) decl
