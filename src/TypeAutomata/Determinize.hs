839e5be24cc0aa1fbf2c939302181c2eab4b7045
BinderDavid <binderd@informatik.uni-tuebingen.de>
2021-07-21 13:49:19 +0200

    Simplify determinization

--- src/TypeAutomata/Determinize.hs
+++ src/TypeAutomata/Determinize.hs
@@ -7,60 +7,62 @@ import Data.Graph.Inductive.PatriciaTree
 import Data.List.NonEmpty (NonEmpty(..))
 import Data.Map (Map)
 import qualified Data.Map as M
-import Data.Maybe (catMaybes)
 import Data.Set (Set)
 import qualified Data.Set as S
 
 import Syntax.Types
 import TypeAutomata.Definition
 import Utils
+import Data.Maybe (mapMaybe)
 
 ---------------------------------------------------------------------------------------
--- Generic determinization algorithm
+-- First step of determinization:
+-- Compute the new transition function of the determinized graph,
+-- using the powerset construction.
 ---------------------------------------------------------------------------------------
 
-getAlphabetForNodes :: Ord b => Gr NodeLabel b -> Set Node -> [b]
-getAlphabetForNodes gr ns = nub $ map (\(_,_,b) -> b) (concat (map (out gr) (S.toList ns)))
+-- | A transition function for the powerset construction
+type TransFun = Map (Set Node) [(Set Node, EdgeLabelNormal)]
 
-succsWith :: Eq b => Gr NodeLabel b -> Set Node -> b -> Set Node
-succsWith gr ns x = S.fromList $ map fst . filter ((==x).snd) . concat $ map (lsuc gr) (S.toList ns)
+-- | Collect all (unique) outgoing edgelabels from the given set of nodes.
+getAlphabetForNodes :: Gr NodeLabel EdgeLabelNormal -> Set Node -> [EdgeLabelNormal]
+getAlphabetForNodes gr ns = nub $ map (\(_,_,b) -> b) (concatMap (out gr) (S.toList ns))
 
-determinizeState :: Ord b => [Set Node] -> Gr NodeLabel b -> State (Map (Set Node) [((Set Node),b)]) ()
-determinizeState [] _ = return ()
+-- | Get all successor nodes from the given set which are connected by the given edgeLabel.
+succsWith :: Gr NodeLabel EdgeLabelNormal -> Set Node -> EdgeLabelNormal -> Set Node
+succsWith gr ns x = S.fromList $ map fst . filter ((==x).snd) $ concatMap (lsuc gr) (S.toList ns)
+
+determinizeState :: [Set Node]
+                 -> Gr NodeLabel EdgeLabelNormal
+                 -> State TransFun ()
+determinizeState [] _ = pure ()
 determinizeState (ns:rest) gr = do
   mp <- get
   if ns `elem` M.keys mp then determinizeState rest gr
     else do
-      let newEdges = map (\x -> (succsWith gr ns x, x)) (getAlphabetForNodes gr ns)
-      let newNodeSets = map fst newEdges
+      let alphabet = getAlphabetForNodes gr ns
+      let newEdges = map (\x -> (succsWith gr ns x, x)) alphabet
       modify (M.insert ns newEdges)
+      let newNodeSets = map fst newEdges
       determinizeState (newNodeSets ++ rest) gr
 
-runDeterminize :: Ord b => Gr NodeLabel b -> [Node] -> Map (Set Node) [((Set Node),b)]
-runDeterminize gr starts = snd $ runState (determinizeState [S.fromList starts] gr) M.empty
+-- | The determinization algorithm starts with the set of starting nodes.
+runDeterminize :: Gr NodeLabel EdgeLabelNormal
+               -> [Node] -- ^ Starting states
+               -> TransFun
+runDeterminize gr starts = execState (determinizeState [S.fromList starts] gr) M.empty
 
-getNewNodeLabel :: ([NodeLabel] -> NodeLabel) -> Gr NodeLabel b -> Set Node -> NodeLabel
-getNewNodeLabel f gr ns = f $ catMaybes (map (lab gr) (S.toList ns))
+---------------------------------------------------------------------------------------
+-- Second step: Compute a new type graph from the TransFun.
+---------------------------------------------------------------------------------------
 
--- first argument is the node label "combiner"
--- second result argument is a mapping from sets of node ids to new node ids
--- this is necessary to correctly handle flow edges, which is done later
-determinize' :: Ord b => ([NodeLabel] -> NodeLabel) -> (Gr NodeLabel b, [Node]) -> (Gr NodeLabel b, Node, [(Node, (Set Node))])
-determinize' f (gr,starts) =
-  let
-    mp = runDeterminize gr starts
-  in ( mkGraph [(i, getNewNodeLabel f gr ns) | (ns,_) <- M.toList mp, let i = M.findIndex ns mp]
-               [(i, M.findIndex ns' mp,el) | (ns,es) <- M.toList mp, let i = M.findIndex ns mp, (ns',el) <- es]
-     , M.findIndex (S.fromList starts) mp
-     , [(M.findIndex nodeSet mp, nodeSet) | (nodeSet,_) <- M.toList mp])
-
-------------------------------------------------------------------------------
--- Applied to type graphs
-------------------------------------------------------------------------------
+-- | Return the combined node label for the given set of nodes.
+getNewNodeLabel :: Gr NodeLabel b -> Set Node -> NodeLabel
+getNewNodeLabel gr ns = combineNodeLabels $ mapMaybe (lab gr) (S.toList ns)
 
 combineNodeLabels :: [NodeLabel] -> NodeLabel
 combineNodeLabels nls
-  = if not . allEq $ (map nl_pol nls)
+  = if not . allEq $ map nl_pol nls
       then error "Tried to combine node labels of different polarity!"
       else MkNodeLabel {
         nl_pol = pol,
@@ -75,10 +77,27 @@ combineNodeLabels nls
     mrgCodat [] = Nothing
     mrgCodat (xtor:xtors) = Just $ case pol of {Pos -> intersections (xtor :| xtors); Neg -> S.unions (xtor:xtors)}
 
+determinize' :: (Gr NodeLabel EdgeLabelNormal, [Node])
+             -> ( Gr NodeLabel EdgeLabelNormal
+                , Node --  New start node
+                , [(Node, Set Node)] --  a mapping from sets of node ids to new node ids (this is necessary to correctly handle flow edges, which is done later)
+                )
+determinize' (gr,starts) =
+  let
+    mp = runDeterminize gr starts
+  in ( mkGraph [(i, getNewNodeLabel gr ns) | (ns,_) <- M.toList mp, let i = M.findIndex ns mp]
+               [(i, M.findIndex ns' mp,el) | (ns,es) <- M.toList mp, let i = M.findIndex ns mp, (ns',el) <- es]
+     , M.findIndex (S.fromList starts) mp
+     , [(M.findIndex nodeSet mp, nodeSet) | (nodeSet,_) <- M.toList mp])
+
+------------------------------------------------------------------------------
+-- Lift the determinization algorithm to type graphs.
+------------------------------------------------------------------------------
+
 determinize :: TypeAut pol -> TypeAutDet pol
 determinize TypeAut{ ta_pol, ta_starts, ta_core = TypeAutCore { ta_gr, ta_flowEdges }} =
   let
-    (newgr, newstart, mp) = determinize' combineNodeLabels (ta_gr, ta_starts)
+    (newgr, newstart, mp) = determinize' (ta_gr, ta_starts)
     newFlowEdges = [(i,j) | (i,ns) <- mp, (j,ms) <- mp,
                             not $ null [(n,m) | n <- S.toList ns, m <- S.toList ms, (n,m) `elem` ta_flowEdges]]
   in
