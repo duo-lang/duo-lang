66ffb8938b6fdff701ab60b3b7051dfd096295bf
BinderDavid <binderd@informatik.uni-tuebingen.de>
2021-09-29 18:18:30 +0200

    Update extensioN

--- vscode-plugin/dualsub-client/src/extension.ts
+++ vscode-plugin/dualsub-client/src/extension.ts
@@ -1,25 +1,50 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as vscodelang from 'vscode-languageclient';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let client: vscodelang.LanguageClient;
+
+// This method is called when the "dualsub-client" extension is activated.
 export function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "dualsub-client" is now active!');
+    
+	let config = vscode.workspace.getConfiguration("dualsub");
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('dualsub-client.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from dualsub-client!');
-	});
+	let defaultCmd = "dualsub";
+	let dualsubCmd = config.get<string>("executable") || defaultCmd;
+	let args = ["lsp"];
 
-	context.subscriptions.push(disposable);
+	let serverOptions: vscodelang.ServerOptions = {
+        run: {
+            command: dualsubCmd,
+            args: args,
+            options: {}
+        },
+        debug: {
+            command: dualsubCmd,
+            args: args.concat(["--debug"]), // Not implemented in the LSP Server!
+            options: {}
+        }
+    };
+
+    let clientOptions: vscodelang.LanguageClientOptions = {
+        documentSelector: [{
+            scheme: 'file',
+            language: 'dualsub'
+        }],
+        diagnosticCollectionName: "dualsub"
+    };
+
+    client = new vscodelang.LanguageClient(
+        'dualsubLanguageServer',
+        'DualSub Language Server',
+        serverOptions,
+        clientOptions
+    );
+
+	context.subscriptions.push(client.start());
 }
 
 // this method is called when your extension is deactivated
