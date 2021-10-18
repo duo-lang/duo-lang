5bb45a94f80495a0d40263910d1f6de66b4deb9b
David <david.laewen@gmx.de>
2021-10-18 15:08:14 +0200

    Add translation monad and cleanup function

--- src/TypeTranslation.hs
+++ src/TypeTranslation.hs
@@ -6,6 +6,8 @@ import Control.Monad.State
 import Data.Maybe
 import Data.Map (Map)
 import qualified Data.Map as M
+import Data.Set
+import qualified Data.Set as S
 
 import Errors
 import Lookup
@@ -20,20 +22,22 @@ import Syntax.CommonTerm
 -- We store mappings of recursive type variables
 ---------------------------------------------------------------------------------------------
 
-newtype TranslateState = TranslateState { recVarMap :: Map TypeName TVar }
+data TranslateState = TranslateState 
+  { recVarMap :: Map TypeName TVar
+  , recVarsUsed :: Set TVar }
 
 initialState :: TranslateState
-initialState = TranslateState { recVarMap = M.empty }
+initialState = TranslateState { recVarMap = M.empty, recVarsUsed = S.empty }
 
 newtype TranslateReader = TranslateReader { context :: [TypArgs Pos] }
 
 initialReader :: Environment FreeVarName -> (Environment FreeVarName, TranslateReader)
 initialReader env = (env, TranslateReader { context = [] })
 
-newtype TraM a = TraM { getTraM :: ReaderT (Environment FreeVarName, TranslateReader) (StateT TranslateState (Except Error)) a }
+newtype TranslateM a = TraM { getTraM :: ReaderT (Environment FreeVarName, TranslateReader) (StateT TranslateState (Except Error)) a }
   deriving (Functor, Applicative, Monad, MonadState TranslateState, MonadReader (Environment FreeVarName, TranslateReader), MonadError Error)
 
-runTraM :: Environment FreeVarName -> TraM a -> Either Error (a, TranslateState)
+runTraM :: Environment FreeVarName -> TranslateM a -> Either Error (a, TranslateState)
 runTraM env m = case runExcept (runStateT (runReaderT (getTraM m) (initialReader env)) initialState) of
   Left err -> Left err
   Right (x, state) -> Right (x, state)
@@ -42,31 +46,74 @@ runTraM env m = case runExcept (runStateT (runReaderT (getTraM m) (initialReader
 -- Helper functions
 ---------------------------------------------------------------------------------------------
 
--- | Make an XtorName structural
-xtorNameMakeStructural :: XtorName -> XtorName
-xtorNameMakeStructural (MkXtorName _ s) = MkXtorName Structural s
+modifyVarMap :: (Map TypeName TVar -> Map TypeName TVar) -> TranslateM ()
+modifyVarMap f = do
+  modify (\TranslateState{..} -> 
+    TranslateState{ recVarMap = f recVarMap, recVarsUsed })
+
+modifyVarSet :: (Set TVar -> Set TVar) -> TranslateM ()
+modifyVarSet f = do
+  modify (\TranslateState{..} ->
+    TranslateState{ recVarMap, recVarsUsed = f recVarsUsed })
+
+-- | Translate all producer and consumer types in an xtor signature
+translateSigArgs :: XtorSig pol -> TranslateM (XtorSig pol)
+translateSigArgs MkXtorSig{..} = do
+  pts' <- mapM translateToStructural' $ prdTypes sig_args
+  cts' <- mapM translateToStructural' $ cnsTypes sig_args
+  return $ MkXtorSig (xtorNameMakeStructural sig_name) (MkTypArgs pts' cts')
+    where
+      xtorNameMakeStructural :: XtorName -> XtorName
+      xtorNameMakeStructural (MkXtorName _ s) = MkXtorName Structural s
 
 -- | Translate a nominal type into a structural type recursively
-translateToStructural' :: Integer -> Typ pol -> TraM (Typ pol)
-translateToStructural' i (TyNominal pr tn) = do
+translateToStructural' :: Typ pol -> TranslateM (Typ pol)
+translateToStructural' (TyNominal pr tn) = do
   m <- gets recVarMap
-  -- If current type name contained in cache, return recursive variable
-  if M.member tn m then return $ TyVar pr $ fromJust (M.lookup tn m)
+  -- If current type name contained in cache, return corresponding rec. type variable
+  if M.member tn m then do
+    let tv = fromJust (M.lookup tn m)
+    modifyVarSet $ S.insert tv -- add rec. type variable to used var cache
+    return $ TyVar pr tv
   else do
     NominalDecl{..} <- lookupTypeName tn
-    let tv = MkTVar ("r" <> ppPrint (show i))
+    let tv = MkTVar ("r" <> ppPrint (show 1)) -- TODO: get fresh rec. tvar
+    -- Insert current type name into cache with corresponding rec. type variable
+    modifyVarMap $ M.insert tn tv
     case data_polarity of
       Data -> do
-        -- let prdts = translateToStructural' (i+1) (M.insert tv tn m) <$> prdTypes . sig_args <$> data_xtors pr
-        --
-        xtorSig <- forM (data_xtors pr) (\MkXtorSig{..} -> do
-            pts' <- mapM (translateToStructural' i) $ prdTypes sig_args
-            cts' <- mapM (translateToStructural' i) $ cnsTypes sig_args
-            return $ MkXtorSig (xtorNameMakeStructural sig_name) (MkTypArgs pts' cts')
-            )
-        return $ TyData pr xtorSig
+        xtss <- mapM translateSigArgs $ data_xtors pr
+        return $ TyRec pr tv $ TyData pr xtss
       Codata -> do
-        let xtorSig = xtorSigMakeStructural <$> data_xtors (flipPolarityRep pr)
-        return $ TyCodata pr xtorSig
-translateToStructural' _ _ = do
+        xtss <- mapM translateSigArgs $ data_xtors $ flipPolarityRep pr
+        return $ TyRec pr tv $ TyCodata pr xtss
+translateToStructural' _ = do
   throwOtherError ["Can only translate nominal types"]
+
+-- | Remove unused recursion headers
+cleanUp :: Typ pol -> TranslateM (Typ pol)
+cleanUp ty = case ty of
+  -- Remove outermost recursive type if its variable is unused
+  TyRec pr tv ty' -> do
+    s <- gets recVarsUsed
+    ty'Cleaned <- cleanUp ty'
+    if S.member tv s then return $ TyRec pr tv ty'Cleaned
+    else return ty'Cleaned
+  -- Propagate cleanup in arg types of xtor signature
+  TyData pr xtss -> do
+    xtss' <- mapM cleanUpXtorSig xtss
+    return $ TyData pr xtss'
+  -- Propagate cleanup in arg types of xtor signature
+  TyCodata pr xtss -> do
+    xtss' <- mapM cleanUpXtorSig xtss
+    return $ TyCodata pr xtss'
+  -- Type variables remain unchanged
+  tyVar@TyVar{} -> return tyVar
+  -- Other types imply incorrect translation
+  _ -> throwOtherError ["Nominal type translation: Cleanup error"]
+  where
+    cleanUpXtorSig :: XtorSig pol -> TranslateM (XtorSig pol)
+    cleanUpXtorSig MkXtorSig{..} = do
+      pts <- mapM cleanUp $ prdTypes sig_args
+      cts <- mapM cleanUp $ cnsTypes sig_args
+      return MkXtorSig{ sig_name, sig_args = MkTypArgs pts cts }
