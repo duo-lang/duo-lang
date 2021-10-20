f75d151adf21ce908c72244813706bc94b3b97b9
BinderDavid <binderd@informatik.uni-tuebingen.de>
2021-10-20 11:16:15 +0200

    Generate translation code actions for defdecls

--- app/LSP/CodeActionHandler.hs
+++ app/LSP/CodeActionHandler.hs
@@ -12,10 +12,10 @@ import Control.Monad.IO.Class ( MonadIO(liftIO) )
 
 import LSP.Definition ( LSPMonad )
 import LSP.MegaparsecToLSP ( locToRange )
-import Syntax.CommonTerm ( FreeVarName, PrdCns(Prd) )
 import Syntax.Program
-    ( Declaration(PrdDecl), Environment(prdEnv), IsRec(Recursive) )
+    ( Declaration(PrdDecl), Environment(prdEnv, defEnv), IsRec(Recursive) )
 import Syntax.Types ( Polarity(Pos), TypeScheme )
+import Syntax.ATerms
 import Syntax.STerms ( createNamesSTerm, STerm )
 import TypeInference.Driver
     ( defaultInferenceOptions,
@@ -29,6 +29,7 @@ import Pretty.Pretty ( ppPrint, NamedRep(NamedRep) )
 import Pretty.Program ()
 import Eval.Eval ( EvalOrder(..) )
 import Translate.Focusing ( focusSTerm, isFocusedSTerm )
+import Translate.Translate ( compile )
 
 
 ---------------------------------------------------------------------------------
@@ -57,26 +58,32 @@ codeActionHandler = requestHandler STextDocumentCodeAction $ \req responder -> d
 
 generateCodeActions :: TextDocumentIdentifier -> Environment FreeVarName -> List (Command  |? CodeAction)
 generateCodeActions ident env = do
-  let prds = M.toList  $ M.filter (\(tm,_,_) -> not (isFocusedSTerm CBV tm)) $ prdEnv env
-  let cbvFocusActions = [ generateCodeAction ident CBV prd | prd@(_,(tm,_,_)) <- prds, not (isFocusedSTerm CBV tm)]
-  let cbnFocusActions = [ generateCodeAction ident CBN prd | prd@(_,(tm,_,_)) <- prds, not (isFocusedSTerm CBN tm)]
-  List (cbvFocusActions <> cbnFocusActions)
+  let prds = M.toList $ prdEnv env
+  let cbvFocusActions = [ generateFocusCodeAction ident CBV prd | prd@(_,(tm,_,_)) <- prds, not (isFocusedSTerm CBV tm)]
+  let cbnFocusActions = [ generateFocusCodeAction ident CBN prd | prd@(_,(tm,_,_)) <- prds, not (isFocusedSTerm CBN tm)]
+  let defs = M.toList $ defEnv env
+  let translateActions = generateTranslateCodeAction ident <$> defs
+  List (cbvFocusActions <> cbnFocusActions <> translateActions)
 
-generateCodeAction :: TextDocumentIdentifier -> EvalOrder -> (FreeVarName, (STerm Prd () FreeVarName, Loc, TypeScheme Pos)) -> Command |? CodeAction
-generateCodeAction ident eo arg@(name, _)= InR $ CodeAction { _title = "Focus " <> (case eo of CBV -> "CBV "; CBN -> "CBN ") <> name
-                                                            , _kind = Just CodeActionQuickFix 
-                                                            , _diagnostics = Nothing
-                                                            , _isPreferred = Nothing
-                                                            , _disabled = Nothing
-                                                            , _edit = Just (generateEdit eo ident arg)
-                                                            , _command = Nothing
-                                                            , _xdata = Nothing
-                                                            }
+---------------------------------------------------------------------------------
+-- Provide Focus Actions
+---------------------------------------------------------------------------------
+
+generateFocusCodeAction :: TextDocumentIdentifier -> EvalOrder -> (FreeVarName, (STerm Prd () FreeVarName, Loc, TypeScheme Pos)) -> Command |? CodeAction
+generateFocusCodeAction ident eo arg@(name, _) = InR $ CodeAction { _title = "Focus " <> (case eo of CBV -> "CBV "; CBN -> "CBN ") <> name
+                                                                  , _kind = Just CodeActionQuickFix 
+                                                                  , _diagnostics = Nothing
+                                                                  , _isPreferred = Nothing
+                                                                  , _disabled = Nothing
+                                                                  , _edit = Just (generateFocusEdit eo ident arg)
+                                                                  , _command = Nothing
+                                                                  , _xdata = Nothing
+                                                                  }
 
                                       
 
-generateEdit :: EvalOrder -> TextDocumentIdentifier ->  (FreeVarName, (STerm Prd () FreeVarName, Loc, TypeScheme Pos)) -> WorkspaceEdit
-generateEdit eo (TextDocumentIdentifier uri) (name,(tm,loc,ty)) =
+generateFocusEdit :: EvalOrder -> TextDocumentIdentifier ->  (FreeVarName, (STerm Prd () FreeVarName, Loc, TypeScheme Pos)) -> WorkspaceEdit
+generateFocusEdit eo (TextDocumentIdentifier uri) (name,(tm,loc,ty)) =
   let
     newDecl = NamedRep $ PrdDecl Recursive () name (Just ty) (createNamesSTerm (focusSTerm eo tm))
     replacement = ppPrint newDecl
@@ -86,3 +93,29 @@ generateEdit eo (TextDocumentIdentifier uri) (name,(tm,loc,ty)) =
                   , _documentChanges = Nothing
                   , _changeAnnotations = Nothing
                   }
+
+---------------------------------------------------------------------------------
+-- Provide Translation Actions
+---------------------------------------------------------------------------------
+
+generateTranslateCodeAction :: TextDocumentIdentifier -> (FreeVarName,(ATerm () FreeVarName, Loc, TypeScheme Pos)) -> Command |? CodeAction
+generateTranslateCodeAction ident arg@(name,_) = InR $ CodeAction { _title = "Translate " <> name
+                                                                  , _kind = Just CodeActionQuickFix 
+                                                                  , _diagnostics = Nothing
+                                                                  , _isPreferred = Nothing
+                                                                  , _disabled = Nothing
+                                                                  , _edit = Just (generateTranslateEdit ident arg)
+                                                                  , _command = Nothing
+                                                                  , _xdata = Nothing
+                                                                  }
+
+generateTranslateEdit :: TextDocumentIdentifier  -> (FreeVarName,(ATerm () FreeVarName, Loc, TypeScheme Pos)) -> WorkspaceEdit 
+generateTranslateEdit (TextDocumentIdentifier uri) (name, (tm,loc,ty)) = 
+  let
+    newDecl = NamedRep $ PrdDecl Recursive () name (Just ty) (createNamesSTerm (compile tm))
+    replacement = ppPrint newDecl
+    edit = TextEdit {_range=locToRange loc, _newText=replacement}
+  in
+    WorkspaceEdit { _changes= Just (Map.singleton uri (List [edit]))
+                  , _documentChanges=Nothing 
+                  , _changeAnnotations=Nothing}
