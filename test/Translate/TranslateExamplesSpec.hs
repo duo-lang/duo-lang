2a4e40999b507292047455023936adda0840e49e
BinderDavid <binderd@informatik.uni-tuebingen.de>
2021-11-11 14:30:50 +0100

    Fix tests in TranslateExampleSpec

--- test/Translate/TranslateExamplesSpec.hs
+++ test/Translate/TranslateExamplesSpec.hs
@@ -33,10 +33,10 @@ spec = do
         isClosed "Succ(Succ(Zero))"
         isClosed "True"
         -- complex examples
-        isClosed "comatch { Ap(x,y) => match x with {Zero => True, Succ(x) => y}}"
-        isClosed "comatch { Ap(x) => match x with {Zero => x, Succ(z1,z2) => z1}}"
-        isClosed "comatch { Ap(x,y) => match x with { Zero => Add(x,y), Succ(z1) => match z1 with { Zero => z1 , Succ(z2) =>  Add(x,z2)} }}"
-        isClosed "comatch { Ap(x) => match x with { Zero => match x with { Zero => True }}}"
+        isClosed "cocase { Ap(x,y) => case x of {Zero => True, Succ(x) => y}}"
+        isClosed "cocase { Ap(x) => case x of {Zero => x, Succ(z1,z2) => z1}}"
+        isClosed "cocase { Ap(x,y) => case x of { Zero => Add(x,y), Succ(z1) => case z1 of { Zero => z1 , Succ(z2) =>  Add(x,z2)} }}"
+        isClosed "cocase { Ap(x) => case x of { Zero => case x of { Zero => True }}}"
 
       describe "Check results of compile function:" $ do
         -- values
@@ -44,16 +44,16 @@ spec = do
         compileExample "Succ(Succ(Zero))" "Succ(Succ(Zero))"
         compileExample "True" "True"
         -- complex examples
-        compileExample "comatch { Ap(x,y) => match x with {Zero => True, Succ(x) => y}}"
+        compileExample "cocase { Ap(x,y) => case x of {Zero => True, Succ(x) => y}}"
                        "comatch { Ap(x,y)[k] => (mu m. x >> match {Zero => True >> m, Succ(z) => y >> m}) >> k}"
-        compileExample "comatch { Ap(x) => match x with {Zero => x, Succ(z1,z2) => z1}}"
+        compileExample "cocase { Ap(x) => case x of {Zero => x, Succ(z1,z2) => z1}}"
                        "comatch { Ap(x)[k] => (mu m. x >> match {Zero => x >> m , Succ(z1,z2) => z1 >> m}) >> k}"
-        compileExample "comatch { Ap(x,y) => match x with { Zero => Add(x,y), Succ(z1) => match z1 with { Zero => z1 , Succ(z2) =>  Add(x,z2)} }}"
+        compileExample "cocase { Ap(x,y) => case x of { Zero => Add(x,y), Succ(z1) => case z1 of { Zero => z1 , Succ(z2) =>  Add(x,z2)} }}"
                        "comatch { Ap(x,y)[k] => (mu m. x >> match {Zero => Add(x, y) >> m, Succ(z1) => (mu m.z1 >> match {Zero => z1 >> m, Succ(z2) => Add(x, z2) >> m} ) >> m }) >> k }"
-        compileExample "comatch { Ap(x) => match x with { Zero => match x with { Zero => True }}}"
+        compileExample "cocase { Ap(x) => case x of { Zero => case x of { Zero => True }}}"
                        "comatch { Ap(x)[k] => (mu m1. x >> match { Zero => (mu m2. x >> match {Zero => True >> m2}) >> m1} ) >> k}"
         compileExample "True.Ap(False)" "mu k. True >> Ap(False)[k]"
-        compileExample "comatch {Ap(x,y) => x.Ap2(y) }"
+        compileExample "cocase {Ap(x,y) => x.Ap2(y) }"
                        "comatch { Ap(x,y)[k] => (mu k2. x >> Ap2(y)[k2]) >> k }"
-        compileExample "comatch {Ap(x,y,z) => z.Ap(x,y)}"
+        compileExample "cocase {Ap(x,y,z) => z.Ap(x,y)}"
                        "comatch { Ap(x,y,z)[k] => (mu m.z >> Ap(x,y)[m]) >> k}"
