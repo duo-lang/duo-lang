c0a185a5cf561cdf7adacf54d4a9921a118c70c9
BinderDavid <binderd@informatik.uni-tuebingen.de>
2021-11-16 12:59:54 +0100

    Fix prettyprinting of types

--- src/Pretty/Types.hs
+++ src/Pretty/Types.hs
@@ -5,6 +5,7 @@ import Prettyprinter
 import Pretty.Pretty
 import Syntax.Types
 import Syntax.Kinds
+import Syntax.CommonTerm
 
 ---------------------------------------------------------------------------------
 -- Symbols used in the prettyprinting of types
@@ -83,8 +84,27 @@ instance PrettyAnn (Typ pol) where
   prettyAnn (TyCodata pr (Just tn) xtors) = dbraces' mempty [prettyAnn tn <+> refinementSym, prettyAnn (TyCodata pr Nothing xtors)]
 
 instance PrettyAnn (PrdCnsType pol) where
-  prettyAnn (PrdType ty) = parens $ prettyAnn ty
-  prettyAnn (CnsType ty) = parens $ prettyAnn ty
+  prettyAnn (PrdType ty) = prettyAnn ty
+  prettyAnn (CnsType ty) = prettyAnn ty
+
+split :: LinearContext pol -> [(PrdCns, LinearContext pol)]
+split [] = []
+split (PrdType ty :rest) = reverse $ split' Prd rest [PrdType ty] []
+split (CnsType ty :rest) = reverse $ split' Cns rest [CnsType ty] []
+
+split' :: PrdCns -> LinearContext pol -> LinearContext pol -> [(PrdCns, LinearContext pol)] -> [(PrdCns, LinearContext pol)]
+split' pc [] ctxt accum = (pc,ctxt):accum
+split' Prd (PrdType ty:rest) ctxt accum = split' Prd rest (PrdType ty:ctxt) accum
+split' Prd (CnsType ty:rest) ctxt accum = split' Cns rest [CnsType ty] ((Prd, reverse ctxt):accum)
+split' Cns (CnsType ty:rest) ctxt accum = split' Cns rest (CnsType ty:ctxt) accum
+split' Cns (PrdType ty:rest) ctxt accum = split' Prd rest [PrdType ty] ((Cns, reverse ctxt):accum)
+
+printSegment :: (PrdCns, LinearContext pol) -> Doc Annotation
+printSegment (Prd, ctxt) = parens'   comma (prettyAnn <$> ctxt)
+printSegment (Cns, ctxt) = brackets' comma (prettyAnn <$> ctxt)
+
+instance {-# OVERLAPPING #-} PrettyAnn (LinearContext pol) where
+  prettyAnn ctxt = mconcat (printSegment <$> split ctxt)
 
 instance PrettyAnn (XtorSig a) where
   prettyAnn (MkXtorSig xt args) = prettyAnn xt <> prettyAnn args
