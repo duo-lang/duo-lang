0eee470a78635fadd9cdc7fae3221d9562b5d02c
BinderDavid <binderd@informatik.uni-tuebingen.de>
2021-11-29 17:16:29 +0100

    Implement zonking on the term level

--- src/Syntax/Zonking.hs
+++ src/Syntax/Zonking.hs
@@ -3,6 +3,8 @@ module Syntax.Zonking where
 import Data.Map (Map)
 import Data.Map qualified as M
 
+import Syntax.CommonTerm
+import Syntax.Terms
 import Syntax.Types
 
 --------------------------------------------------------------------------------
@@ -12,7 +14,7 @@ import Syntax.Types
 newtype Bisubstitution = MkBisubstitution { unBisubstitution :: Map TVar (Typ Pos, Typ Neg) }
 
 ---------------------------------------------------------------------------------
--- Zonking
+-- Zonking of Types
 ---------------------------------------------------------------------------------
 
 zonkType :: Bisubstitution -> Typ pol -> Typ pol
@@ -38,4 +40,40 @@ zonkLinearCtxt bisubst = fmap (zonkPrdCnsType bisubst)
 zonkXtorSig :: Bisubstitution -> XtorSig pol -> XtorSig pol
 zonkXtorSig bisubst (MkXtorSig name ctxt) =
     MkXtorSig name (zonkLinearCtxt bisubst ctxt)
-    
+
+---------------------------------------------------------------------------------
+-- Zonking of Terms
+---------------------------------------------------------------------------------
+
+zonkTerm :: Bisubstitution -> Term pc Inferred -> Term pc Inferred
+zonkTerm bisubst (BoundVar (loc,ty) rep idx) =
+    BoundVar (loc, zonkType bisubst ty) rep idx
+zonkTerm bisubst (FreeVar  (loc,ty) rep nm)  =
+    FreeVar  (loc, zonkType bisubst ty) rep nm
+zonkTerm bisubst (XtorCall (loc,ty) rep xn subst) =
+    XtorCall (loc, zonkType bisubst ty) rep xn (zonkPCTerm bisubst <$> subst)
+zonkTerm bisubst (XMatch (loc,ty) rep ns cases) =
+    XMatch (loc, zonkType bisubst ty) rep ns (zonkSCase bisubst <$> cases)
+zonkTerm bisubst (MuAbs (loc,ty) rep fv cmd) =
+    MuAbs (loc, zonkType bisubst ty) rep fv (zonkCommand bisubst cmd)
+zonkTerm bisubst (Dtor (loc,ty) xt prd subst) =
+    Dtor (loc, zonkType bisubst ty) xt (zonkTerm bisubst prd) (zonkPCTerm bisubst <$> subst)
+zonkTerm bisubst (Match (loc,ty) ns prd cases) =
+    Match (loc, zonkType bisubst ty) ns (zonkTerm bisubst prd) (zonkACase bisubst <$> cases)
+zonkTerm bisubst (Comatch (loc,ty) ns cases) =
+    Comatch (loc, zonkType bisubst ty) ns (zonkACase bisubst <$> cases)
+
+zonkPCTerm :: Bisubstitution -> PrdCnsTerm Inferred -> PrdCnsTerm Inferred
+zonkPCTerm bisubst (PrdTerm tm) = PrdTerm (zonkTerm bisubst tm)
+zonkPCTerm bisubst (CnsTerm tm) = CnsTerm (zonkTerm bisubst tm)
+
+zonkSCase :: Bisubstitution -> SCase Inferred -> SCase Inferred
+zonkSCase bisubst (MkSCase loc nm args cmd) = MkSCase loc nm args (zonkCommand bisubst cmd)
+
+zonkACase :: Bisubstitution -> ACase Inferred -> ACase  Inferred
+zonkACase bisubst (MkACase loc nm args tm) = MkACase loc nm args (zonkTerm bisubst tm)
+
+zonkCommand :: Bisubstitution -> Command Inferred -> Command Inferred
+zonkCommand bisubst (Apply ext prd cns) = Apply ext (zonkTerm bisubst prd) (zonkTerm bisubst cns)
+zonkCommand bisubst (Print ext prd) = Print ext (zonkTerm bisubst prd)
+zonkCommand _       (Done ext) = Done ext
