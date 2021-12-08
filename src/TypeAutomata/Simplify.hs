a2d9ba62120083e3fd9a3c4a04da516b295c0813
Ingo Skupin <ingo.skupin@gmail.com>
2021-12-08 18:26:32 +0100

    separate function names in names for graph jpgs

--- src/TypeAutomata/Simplify.hs
+++ src/TypeAutomata/Simplify.hs
@@ -56,18 +56,18 @@ simplify tys print str = do
     -- Remove epsilon edges
     let typeAutDet = removeEpsilonEdges typeAut
     lint typeAutDet
-    printGraph print ("0_typeAut_" <> str) typeAutDet
+    printGraph print ("0_typeAut" <> "_" <> str) typeAutDet
     -- Determinize the automaton
     let typeAutDet' = determinize typeAutDet
     lint typeAutDet'
-    printGraph print ("1_typeAutDet" <> str) typeAutDet'
+    printGraph print ("1_typeAutDet" <> "_"  <> str) typeAutDet'
     -- Remove admissable flow edges
     let typeAutDetAdms = removeAdmissableFlowEdges typeAutDet'
     lint typeAutDetAdms
-    printGraph print ("2_typeAutDetAdms" <> str) typeAutDetAdms
+    printGraph print ("2_typeAutDetAdms" <> "_"  <> str) typeAutDetAdms
     -- Minimize automaton
     let typeAutMin = minimize typeAutDetAdms
     lint typeAutMin
-    printGraph print ("3_minTypeAut" <> str) typeAutMin
+    printGraph print ("3_minTypeAut" <> "_"  <> str) typeAutMin
     -- Read back to type
     liftEither $ autToType typeAutMin
