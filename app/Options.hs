d37def896963ea9f426064b57900bb871471c689
BinderDavid <binderd@informatik.uni-tuebingen.de>
2021-12-10 17:58:51 +0100

    Add more info in Parsers

--- app/Options.hs
+++ app/Options.hs
@@ -11,32 +11,65 @@ data Options where
     OptCompile :: FilePath -> Options
     OptVersion :: Options
 
+---------------------------------------------------------------------------------
+-- Commandline options for starting a REPL
+---------------------------------------------------------------------------------
+
 replParser :: Parser Options
 replParser = pure OptRepl
 
 replParserInfo :: ParserInfo Options
-replParserInfo = info (replParser <**> helper) fullDesc
+replParserInfo = info (replParser <**> helper)
+                      ( fullDesc 
+                      <> header "dualsub repl - Start an interactive Repl"
+                      )
+
+---------------------------------------------------------------------------------
+-- Commandline options for starting a LSP session
+---------------------------------------------------------------------------------
 
 lspParser :: Parser Options
 lspParser = pure OptLSP
 
 lspParserInfo :: ParserInfo Options
-lspParserInfo = info (lspParser <**> helper) fullDesc
+lspParserInfo = info (lspParser <**> helper)
+                     ( fullDesc 
+                     <> header "dualsub lsp - Start an LSP session"
+                     <> progDesc "This command starts an LSP session. You should probably only use this if you are an editor or know precisely what you are doing."
+                     )
+
+---------------------------------------------------------------------------------
+-- Commandline options for compiling source files
+---------------------------------------------------------------------------------
 
 compileParser :: Parser Options
-compileParser = OptCompile <$> strOption ( long "filepath" <> metavar "TARGET" <> help "foo")
+compileParser = OptCompile <$> argument str (
+                                        metavar "TARGET"
+                                        <> help "Filepath of the source file."
+                                        )
 
 compileParserInfo :: ParserInfo Options
-compileParserInfo = info (compileParser <**> helper) fullDesc
+compileParserInfo = info (compileParser <**> helper)
+                         (fullDesc <> progDesc "Compile DualSub source files"
+                                   <> header "dualsub compile - ")
+
+---------------------------------------------------------------------------------
+-- Commandline option for showing the current version
+---------------------------------------------------------------------------------
 
 versionParser :: Parser Options
 versionParser = pure OptVersion
 
 versionParserInfo :: ParserInfo Options
-versionParserInfo = info (versionParser <**> helper) fullDesc
+versionParserInfo = info (versionParser <**> helper) 
+                         (fullDesc <> progDesc "Show the version")
+
+---------------------------------------------------------------------------------
+-- Combined commandline parser
+---------------------------------------------------------------------------------
 
 optParser :: Parser Options
-optParser = hsubparser (  command "repl" replParserInfo
+optParser = subparser (  command "repl" replParserInfo
                        <> command "lsp" lspParserInfo
                        <> command "compile" compileParserInfo
                        <> command "version" versionParserInfo
@@ -45,6 +78,6 @@ optParser = hsubparser (  command "repl" replParserInfo
 optParserInfo :: ParserInfo Options
 optParserInfo = info (optParser <**> helper)
   (  fullDesc
-  <> progDesc "DualSub"
-  <> header "dualsub - run, compile and check DualSub programs"
+  <> progDesc "DualSub is a research programming language focused on the study of dualities and subtyping."
+  <> header "dualsub - typecheck, run and compile DualSub programs"
   )
