4aaacc9976eebd7efdfe74eefac589a2e86a1669
Ingo Skupin <ingo.skupin@gmail.com>
2022-01-13 16:43:56 +0100

    add comments to minimization functions

--- src/TypeAutomata/Minimize.hs
+++ src/TypeAutomata/Minimize.hs
@@ -12,8 +12,9 @@ import TypeAutomata.Definition
 getAlphabet :: TypeGr -> [EdgeLabelNormal]
 getAlphabet gr = nub $ map (\(_,_,b) -> b) (labEdges gr)
 
+-- find all predecessors with connecting edge labelled by specified label
 predsWith :: TypeGr -> [Node] -> EdgeLabelNormal -> [Node]
-predsWith gr ns x = nub . map fst . filter ((==x).snd) . concat $ map (lpre gr) ns
+predsWith gr ns x = nub . map fst . filter ((==x).snd) $ concatMap (lpre gr) ns
 
 splitAlong :: TypeGr -> [Node] -> EdgeLabelNormal -> [[Node]] -> [[Node]]
 splitAlong gr ds x ps = do
@@ -22,11 +23,14 @@ splitAlong gr ds x ps = do
   let (p1,p2) = (p `intersect` re, p \\ re)
   if null p1 || null p2 then [p] else [p1,p2]
 
+-- minimize by refining the equivalence
+-- this is done by taking refining the second partition an refining it via the first one
 minimize' :: TypeGr -> [[Node]] -> [[Node]] -> [[Node]]
 minimize' _ [] ps = ps
 minimize' gr (d:ds) ps = minimize' gr (delete d (foldr (splitAlong gr d) (d:ds) (getAlphabet gr)))
                                       (foldr (splitAlong gr d) ps (getAlphabet gr))
 
+-- partition list by equivalence (given as a function)
 myGroupBy :: (a -> a -> Bool) -> [a] -> [[a]]
 myGroupBy _ [] = []
 myGroupBy p (x:xs) = let (xs1,xs2) = partition (p x) xs in (x:xs1) : myGroupBy p xs2
@@ -35,10 +39,12 @@ flowNeighbors :: TypeAutCore EdgeLabelNormal -> Node -> Set Node
 flowNeighbors TypeAutCore { ta_flowEdges } i =
   S.fromList $ [n | (j,n) <- ta_flowEdges, i == j] ++ [n | (n,j) <- ta_flowEdges, i == j]
 
+-- nodes are considered equal if they have the same label and the same neighbors along flow edges
 equalNodes :: TypeAutCore EdgeLabelNormal -> Node -> Node -> Bool
 equalNodes aut@TypeAutCore{ ta_gr } i j =
   (lab ta_gr i == lab ta_gr j) && flowNeighbors aut i == flowNeighbors aut j
 
+-- generate a function that maps each node to the representative of its respective equivalence class
 genMinimizeFun :: TypeAutCore EdgeLabelNormal -> (Node -> Node)
 genMinimizeFun aut@TypeAutCore { ta_gr } = getNewNode
   where
