f982e87fa5b746f329d3f79ea3b3a9ce4ab373e9
BinderDavid <binderd@informatik.uni-tuebingen.de>
2022-02-01 13:35:37 +0100

    Remove lowering from subsumption parser

--- src/Parser/Parser.hs
+++ src/Parser/Parser.hs
@@ -15,20 +15,16 @@ import Parser.Lexer
 import Parser.Program
 import Parser.Terms
 import Parser.Types
-import Syntax.AST.Types
-import Syntax.Lowering.Types
+import Syntax.CST.Types
 
 ---------------------------------------------------------------------------------
 -- Parsing for Repl
 ---------------------------------------------------------------------------------
 
-subtypingProblemP :: Parser (TypeScheme Pos, TypeScheme Pos)
+subtypingProblemP :: Parser (TypeScheme, TypeScheme)
 subtypingProblemP = do
   t1 <- typeSchemeP
   _ <- subtypeSym
   t2 <- typeSchemeP
-  case (lowerTypeScheme PosRep t1, lowerTypeScheme PosRep t2) of
-    (Right res1, Right res2) -> pure (res1, res2)
-    (_,_) -> fail "SubtypingProblemP: Cannot lower types."
-
+  return (t1,t2)
 
