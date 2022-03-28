050383fb31f51d5cafae8a65ac2b647829c8aa13
BinderDavid <binderd@informatik.uni-tuebingen.de>
2022-03-28 18:06:16 +0200

    Working again

--- src/Renamer/Definition.hs
+++ src/Renamer/Definition.hs
@@ -34,7 +34,7 @@ lookupXtor :: Loc -> (XtorName, DataCodata) -> RenamerM (NominalStructural, Arit
 lookupXtor loc xs@(xtor,dc) = do
   symbolTable <- getSymbolTable
   case M.lookup xs (xtorMap symbolTable) of
-    Nothing -> throwError $ OtherError (Just loc) ((case dc of Data -> "Constructor"; Codata -> "Destructor") <>" not in environment: " <> ppPrint xtor)
+    Nothing -> throwError $ OtherError (Just loc) ((case dc of Data -> "Constructor"; Codata -> "Destructor") <>" not in symbol table: " <> ppPrint xtor)
     Just ns -> pure ns
 
 -- | Find the Arity of a given typename
@@ -43,7 +43,7 @@ lookupTypeConstructorAritiy tn = do
     symbolTable <- getSymbolTable
     case M.lookup tn (tyConMap symbolTable) of
         Just (_,polykind) -> pure polykind
-        Nothing -> throwOtherError ["Type name " <> unTypeName tn <> " not found in environment"]
+        Nothing -> throwOtherError ["Type name " <> unTypeName tn <> " not found in symbol table"]
 
 ------------------------------------------------------------------------------
 -- Deprecated stuff
@@ -51,12 +51,10 @@ lookupTypeConstructorAritiy tn = do
 
 updateSymbolTable :: (SymbolTable -> SymbolTable) -> RenamerM ()
 updateSymbolTable f = do
-    env <- getDriverEnv
+    env <- gets driverEnv
     let newEnv = env { symbolTable = f (symbolTable env)}
     modify (\state -> state { driverEnv = newEnv })
 
-getDriverEnv :: RenamerM (Environment Inferred)
-getDriverEnv = gets driverEnv
 
 -- | Given the Library Paths contained in the inference options and a module name,
 -- try to find a filepath which corresponds to the given module name.
@@ -78,13 +76,18 @@ createSymbolTable [] = mempty
 createSymbolTable ((CST.XtorDecl _ dc xt args _):decls) =
   let st = createSymbolTable decls
   in st { xtorMap = M.insert (xt,dc) (Structural, fst <$> args) (xtorMap st)}
-createSymbolTable ((CST.DataDecl _ dd):decls) =
-  let ns = case CST.data_refined dd of
+createSymbolTable ((CST.DataDecl _ CST.NominalDecl { data_refined, data_name, data_polarity, data_kind, data_xtors }):decls) =
+  -- Create the default polykind
+  let polyKind = case data_kind of
+                    Nothing -> MkPolyKind [] [] (case data_polarity of Data -> CBV; Codata -> CBN)
+                    Just knd -> knd
+      ns = case data_refined of
                Refined -> Refinement
                NotRefined -> Nominal
       st = createSymbolTable decls
-      xtors = M.fromList [((CST.sig_name xt, CST.data_polarity dd), (ns, CST.linearContextToArity (CST.sig_args xt)))| xt <- CST.data_xtors dd]
-  in st { xtorMap  = M.union xtors (xtorMap st) }
+      xtors = M.fromList [((CST.sig_name xt, data_polarity), (ns, CST.linearContextToArity (CST.sig_args xt)))| xt <- data_xtors]
+  in st { xtorMap  = M.union xtors (xtorMap st)
+        , tyConMap = M.insert data_name (data_refined, polyKind)(tyConMap st)}
 createSymbolTable (_:decls) = createSymbolTable decls
 
 
