3851eddffc7ec9fb0a71fb80e42ff21e6860c925
BinderDavid <binderd@informatik.uni-tuebingen.de>
2022-04-05 13:07:49 +0200

    Add core terms and programs

---
+++ src/Syntax/Core/Program.hs
@@ -0,0 +1,31 @@
+module Syntax.Core.Program where
+
+import Data.Text (Text)
+
+import Syntax.Common
+import Syntax.Core.Terms( Command )
+import Syntax.RST.Types (  DataDecl )
+import Utils ( Loc )
+
+---------------------------------------------------------------------------------
+-- Declarations
+---------------------------------------------------------------------------------
+
+data Declaration where
+  CmdDecl        :: Loc -> Maybe DocComment -> FreeVarName -> Command                                                       -> Declaration
+  DataDecl       :: Loc -> Maybe DocComment -> DataDecl                                                                     -> Declaration
+  XtorDecl       :: Loc -> Maybe DocComment -> DataCodata -> XtorName -> [(PrdCns, MonoKind)] -> EvaluationOrder            -> Declaration
+  ImportDecl     :: Loc -> Maybe DocComment -> ModuleName                                                                   -> Declaration
+  SetDecl        :: Loc -> Maybe DocComment -> Text                                                                         -> Declaration
+  TyOpDecl       :: Loc -> Maybe DocComment -> TyOpName -> Precedence -> Associativity -> TypeName                          -> Declaration
+  
+
+instance Show Declaration where
+  show (CmdDecl loc doc fv cmd) = "CmdDecl: " ++ show loc ++ show doc ++ show fv ++ show cmd
+  show (DataDecl loc doc dcl)= "DataDecl: " ++ show loc ++ show doc ++ show dcl
+  show (XtorDecl loc doc dc xt args res) = "XtorDecl: " ++ show loc ++ show doc ++ show dc ++ show xt ++ show args ++ show res
+  show (ImportDecl loc doc mn) = "ImportDecl: " ++ show loc ++ show doc ++ show mn
+  show (SetDecl loc doc txt) = "SetDecl: " ++ show loc ++ show doc ++ show txt
+  show (TyOpDecl loc doc op prec assoc ty) = "TyOpDecl: " ++ show loc ++ show doc ++ show op ++ show prec ++ show assoc ++ show ty
+  
+type Program = [Declaration]
