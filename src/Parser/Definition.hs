72b0155cd671823047fe12d615ed4528bb59b6fb
Klaus Ostermann <klaus612@gmail.com>
2022-04-12 01:22:39 +0200

    added parser debug function

--- src/Parser/Definition.hs
+++ src/Parser/Definition.hs
@@ -2,6 +2,7 @@ module Parser.Definition
   ( Parser
   , runInteractiveParser
   , runFileParser
+  , dbg
   ) where
 
 import Control.Applicative (Alternative)
@@ -10,6 +11,7 @@ import Data.Text qualified as T
 import Data.Void (Void)
 import Data.Text (Text)
 import Text.Megaparsec
+import qualified Text.Megaparsec.Debug 
 
 import Errors
 import Utils
@@ -21,6 +23,13 @@ import Utils
 newtype Parser a = Parser { unParser :: Parsec Void Text a }
   deriving (Functor, Applicative, Monad, MonadFail, Alternative, MonadPlus
            , MonadParsec Void Text)
+
+-------------------------------------------------------------------------------------------
+-- Debugging Support 
+-------------------------------------------------------------------------------------------
+
+dbg :: Show a => String   -> Parser a    -> Parser a
+dbg txt (Parser p) = Parser $ Text.Megaparsec.Debug.dbg txt p
 
 -------------------------------------------------------------------------------------------
 -- Translating a Parse Error to an Error
