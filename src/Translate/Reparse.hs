72621c2551acf54ecebab7a279199a6db031ec8e
Qbxs <ip4ssi@gmail.com>
2022-06-28 11:38:07 +0200

    throw error for not implemented type constraints in type scheme

--- src/Translate/Reparse.hs
+++ src/Translate/Reparse.hs
@@ -473,7 +473,7 @@ embedTypeScheme :: RST.TypeScheme pol -> CST.TypeScheme
 embedTypeScheme RST.TypeScheme { ts_loc, ts_vars, ts_monotype } =
   CST.TypeScheme { ts_loc = ts_loc
                  , ts_vars = ts_vars
-                 , ts_constraints = []
+                 , ts_constraints = error "Type constraints not implemented yet for RST type scheme."
                  , ts_monotype = embedType ts_monotype
                  }
 
