b5868f1b5a78f959acf6292d46eec258e75ac5c5
Ingo Skupin <ingo.skupin@gmail.com>
2022-07-08 19:48:39 +0200

    start work on fix for admissible

--- src/TypeAutomata/RemoveAdmissible.hs
+++ src/TypeAutomata/RemoveAdmissible.hs
@@ -5,13 +5,16 @@ module TypeAutomata.RemoveAdmissible
 import Control.Applicative ((<|>))
 import Control.Monad (guard, forM_)
 import Data.Graph.Inductive.Graph
-import Data.List (delete)
-import Data.Maybe (isJust)
+import Data.Maybe (fromMaybe)
 import Data.Set qualified as S
 import Data.Tuple (swap)
 
-import Syntax.Common
+import Syntax.Common.Polarity ( Polarity(Pos, Neg) )
+import Syntax.Common.PrdCns ( PrdCns(Cns, Prd) )
+import Syntax.Common.XData ( DataCodata(Codata, Data) )
 import TypeAutomata.Definition
+import Control.Monad.State.Strict (StateT (runStateT), lift, MonadState, modify, gets)
+import GHC.Base (Alternative)
 
 ----------------------------------------------------------------------------------------
 -- Removal of admissible flow edges.
@@ -60,13 +63,46 @@ import TypeAutomata.Definition
 --
 ----------------------------------------------------------------------------------------
 
-sucWith :: TypeGr -> Node -> EdgeLabelNormal -> Maybe Node
-sucWith gr i el = lookup el (map swap (lsuc gr i))
+data AdmissableS = AdmissableS { memo :: S.Set FlowEdge, blacklist :: S.Set FlowEdge }
+newtype AdmissableM a = AdmissableM { runAdmissable :: StateT AdmissableS Maybe a }
+  deriving (Functor, Applicative, Monad, MonadState AdmissableS, MonadFail, Alternative)
 
-subtypeData :: TypeAutCore EdgeLabelNormal -> FlowEdge -> Maybe ()
+execAdmissable :: AdmissableM a -> (a, AdmissableS)
+execAdmissable = fromMaybe (error "should not happen") . flip runStateT AdmissableS { memo = S.empty, blacklist = S.empty } . runAdmissable
+
+liftAM :: Maybe a -> AdmissableM a
+liftAM = AdmissableM . lift
+
+sucWith :: TypeGr -> Node -> EdgeLabelNormal -> AdmissableM Node
+sucWith gr i el = liftAM $ lookup el (map swap (lsuc gr i))
+
+modifyMemo :: (S.Set FlowEdge -> S.Set FlowEdge) -> AdmissableM ()
+modifyMemo f = modify $ \s -> s { memo = f $ memo s }
+
+insertFE :: FlowEdge -> AdmissableM ()
+insertFE = modifyMemo . S.insert
+
+--  removeFE :: FlowEdge -> AdmissableM ()
+--  removeFE = modifyMemo . S.delete
+
+blacklistFE :: FlowEdge -> AdmissableM ()
+blacklistFE fe =
+  modify $ \s -> s { memo = fe `S.delete` memo s, blacklist = fe `S.insert` blacklist s }
+
+isMemoised :: FlowEdge -> AdmissableM ()
+isMemoised fe = do
+  m <- gets memo
+  guard $ fe `S.member` m
+
+isNotBlacklisted :: FlowEdge -> AdmissableM ()
+isNotBlacklisted fe = do
+  b <- gets blacklist
+  guard $ not $ fe `S.member` b
+
+subtypeData :: TypeAutCore EdgeLabelNormal -> FlowEdge -> AdmissableM ()
 subtypeData aut@TypeAutCore{ ta_gr } (i,j) = do
-  (MkNodeLabel Neg (Just dat1) _ _ _  _ _) <- lab ta_gr i
-  (MkNodeLabel Pos (Just dat2) _ _ _ _ _) <- lab ta_gr j
+  (MkNodeLabel Neg (Just dat1) _ _ _  _ _) <- liftAM $ lab ta_gr i
+  (MkNodeLabel Pos (Just dat2) _ _ _ _ _) <- liftAM $ lab ta_gr j
   -- Check that all constructors in dat1 are also in dat2.
   forM_ (S.toList dat1) $ \xt -> guard (xt `S.member` dat2)
   -- Check arguments of each constructor of dat1.
@@ -78,10 +114,10 @@ subtypeData aut@TypeAutCore{ ta_gr } (i,j) = do
       m <- sucWith ta_gr j el
       admissableM aut (m,n)
 
-subtypeCodata :: TypeAutCore EdgeLabelNormal -> FlowEdge -> Maybe ()
+subtypeCodata :: TypeAutCore EdgeLabelNormal -> FlowEdge -> AdmissableM ()
 subtypeCodata aut@TypeAutCore{ ta_gr } (i,j) = do
-  (MkNodeLabel Neg _ (Just codat1) _ _ _ _) <- lab ta_gr i
-  (MkNodeLabel Pos _ (Just codat2) _ _ _ _) <- lab ta_gr j
+  (MkNodeLabel Neg _ (Just codat1) _ _ _ _) <- liftAM $ lab ta_gr i
+  (MkNodeLabel Pos _ (Just codat2) _ _ _ _) <- liftAM $ lab ta_gr j
   -- Check that all destructors of codat2 are also in codat1.
   forM_ (S.toList codat2) $ \xt -> guard (xt `S.member` codat1)
   -- Check arguments of all destructors of codat2.
@@ -93,24 +129,33 @@ subtypeCodata aut@TypeAutCore{ ta_gr } (i,j) = do
       m <- sucWith ta_gr j el
       admissableM aut (n,m)
 
-subtypeNominal :: TypeAutCore EdgeLabelNormal -> FlowEdge -> Maybe ()
+subtypeNominal :: TypeAutCore EdgeLabelNormal -> FlowEdge -> AdmissableM ()
 subtypeNominal TypeAutCore{ ta_gr } (i,j) = do
-  (MkNodeLabel Neg _ _ nominal1 _ _ _) <- lab ta_gr i
-  (MkNodeLabel Pos _ _ nominal2 _ _ _) <- lab ta_gr j
+  (MkNodeLabel Neg _ _ nominal1 _ _ _) <- liftAM $ lab ta_gr i
+  (MkNodeLabel Pos _ _ nominal2 _ _ _) <- liftAM $ lab ta_gr j
   guard $ not . S.null $ S.intersection nominal1 nominal2
 
-admissableM :: TypeAutCore EdgeLabelNormal -> FlowEdge -> Maybe ()
-admissableM aut@TypeAutCore{..} e =
-  guard (e `elem` ta_flowEdges) <|>
-    subtypeData aut e <|>
-    subtypeCodata aut e <|>
-    subtypeNominal aut e
+admissableM :: TypeAutCore EdgeLabelNormal -> FlowEdge -> AdmissableM ()
+admissableM aut@TypeAutCore{} e =
+  isMemoised e <|>
+    do  isNotBlacklisted e
+        insertFE e
+        subtypeData aut e <|>
+          subtypeCodata aut e <|>
+          subtypeNominal aut e <|>
+          blacklistFE e
 
 -- this version of admissability check also accepts if the edge under consideration is in the set of known flow edges
 -- needs to be seperated for technical reasons...
-admissable :: TypeAutCore EdgeLabelNormal -> FlowEdge -> Bool
-admissable aut@TypeAutCore {..} e = isJust $ admissableM (aut { ta_flowEdges = delete e ta_flowEdges }) e
+--  admissable :: TypeAutCore EdgeLabelNormal -> FlowEdge -> Bool
+--  admissable aut@TypeAutCore {..} e = isJust . execAdmissable $ admissableM aut e
 
 removeAdmissableFlowEdges :: TypeAutDet pol -> TypeAutDet pol
 removeAdmissableFlowEdges aut@TypeAut{ ta_core = tac@TypeAutCore {..}} =
-  aut { ta_core = tac { ta_flowEdges = filter (not . admissable tac) ta_flowEdges }}
+  aut { ta_core = tac { ta_flowEdges = ta_flowEdges_filtered }}
+    where
+      ta_flowEdges_filtered :: [FlowEdge]
+      ta_flowEdges_filtered = filter (`S.member` admissable) ta_flowEdges_filtered
+
+      admissable :: S.Set FlowEdge
+      admissable = memo $ snd $ execAdmissable $ mapM (admissableM tac) ta_flowEdges
