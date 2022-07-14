c6c3e2878532a011ca277d01053a3c22b7a3acd3
Qbxs <ip4ssi@gmail.com>
2022-07-14 10:48:53 +0200

    disable test for method call focussing (not implemented yet)

--- test/Spec/Focusing.hs
+++ test/Spec/Focusing.hs
@@ -19,7 +19,8 @@ import Errors
 type Reason = String
 
 pendingFiles :: [(FilePath, Reason)]
-pendingFiles = []
+pendingFiles = [ ("examples/TypeClasses.ds", "Backend not implemented for type classes")
+               ]
 
 testHelper :: (FilePath, Either (NonEmpty Error) TST.Program) -> EvaluationOrder -> SpecWith ()
 testHelper (example,decls) cbx = describe (show cbx ++ " Focusing the program in  " ++ example ++ " typechecks.") $ 
