3e548556b5748ba2021b3a0b59638374dd2672c0
BinderDavid <binderd@informatik.uni-tuebingen.de>
2022-09-01 19:44:21 +0200

    Add RnFreeVarName

--- src/Syntax/RST/Names.hs
+++ src/Syntax/RST/Names.hs
@@ -1,7 +1,18 @@
-module Syntax.RST.Names where
+module Syntax.RST.Names
+  ( -- Resolved Names
+    RnTypeName(..)
+  , peanoNm
+  , RnFreeVarName(..)
+    -- deBruijn Indizes
+  , Index
+  ) where
 
 import Loc ( defaultLoc, Loc )
 import Syntax.CST.Names qualified as CST
+
+---------------------------------------------------------------------------------
+-- Resolved Names
+---------------------------------------------------------------------------------
 
 -- | Resolved TypeName
 data RnTypeName =
@@ -25,6 +36,21 @@ peanoNm = MkRnTypeName { rnTnLoc    = defaultLoc
                        , rnTnModule = CST.MkModuleName "Peano"
                        , rnTnName   = CST.MkTypeName "Nat"
                        }
+
+-- | Resolved FreeVarName
+data RnFreeVarName =
+    MkRnFreeVarName { rnFvnLoc :: Loc
+                      -- ^ The location of the definition site of the freevar name.
+                    , rnFvnDoc :: Maybe CST.DocComment
+                      -- ^ The comment at the definition site.
+                    , rnFvnFp :: Maybe FilePath
+                      -- ^ The filepath to the definition site.
+                    , rnFvnModule :: CST.ModuleName
+                      -- ^ The module where the freeVarName was defined.
+                    , rnFvnName :: CST.FreeVarName
+                      -- ^ The FreeVarName itself.
+    }
+    deriving (Show, Ord, Eq)
 
 ---------------------------------------------------------------------------------
 -- de Bruijn indices
