1afd3256f30c926c8d7164147a63e22a3bceaa18
MarcoTz <marco.tzschentke@gmail.com>
2022-09-10 14:01:53 +0200

    added error to data decl inference

--- src/TypeInference/GenerateConstraints/Kinds.hs
+++ src/TypeInference/GenerateConstraints/Kinds.hs
@@ -80,42 +80,108 @@ newKVar = do
 defaultKind :: MonoKind
 defaultKind = CBox CBV
 
-annotXtor :: RST.XtorSig pol -> TST.XtorSig pol
-annotXtor (RST.MkXtorSig nm ctxt) = TST.MkXtorSig nm (annotCtxt ctxt)
+annotXtor :: RST.XtorSig pol -> Either Error (TST.XtorSig pol)
+annotXtor (RST.MkXtorSig nm ctxt) = 
+  case annotCtxt ctxt of 
+    Left err -> Left err 
+    Right ctxt' -> Right $ TST.MkXtorSig nm ctxt'
 
-annotCtxt :: RST.LinearContext pol -> TST.LinearContext pol
-annotCtxt [] = []
-annotCtxt (RST.PrdCnsType pc ty:rst) = TST.PrdCnsType  pc (annotTy ty) : annotCtxt rst
+annotCtxt :: RST.LinearContext pol -> Either Error (TST.LinearContext pol)
+annotCtxt [] = Right []
+annotCtxt (RST.PrdCnsType pc ty:rst) = 
+  case annotTy ty of
+    Left err -> Left err 
+    Right ty' -> 
+      case annotCtxt rst of 
+        Left err -> Left err
+        Right ctxt -> Right $ TST.PrdCnsType  pc ty' : ctxt
 
-annotVarTys :: [RST.VariantType pol] -> [TST.VariantType pol]
-annotVarTys [] = [] 
-annotVarTys (RST.CovariantType ty:rst) = TST.CovariantType (annotTy ty) : annotVarTys rst
-annotVarTys (RST.ContravariantType ty:rst) = TST.ContravariantType (annotTy ty) : annotVarTys rst
+annotVarTys :: [RST.VariantType pol] -> Either Error [TST.VariantType pol]
+annotVarTys [] = Right [] 
+annotVarTys (RST.CovariantType ty:rst) = 
+  case annotTy ty of 
+    Left err -> Left err 
+    Right ty' -> 
+      case annotVarTys rst of 
+        Left err -> Left err 
+        Right vartys -> Right $ TST.CovariantType ty' : vartys
+annotVarTys (RST.ContravariantType ty:rst) = 
+  case annotTy ty of 
+    Left err -> Left err 
+    Right ty' -> 
+      case annotVarTys rst of 
+        Left err -> Left err 
+        Right vartys -> Right $ TST.ContravariantType ty' : vartys
+
+collectErrs:: [Either Error a] -> Either Error [a]
+collectErrs [] = Right []
+collectErrs (fst:rst) = case fst of 
+  Left err -> Left err 
+  Right res -> 
+    case collectErrs rst of 
+      Left err -> Left err 
+      Right res' -> Right $ res:res'
 
 
-annotTy :: RST.Typ pol -> TST.Typ pol
-annotTy (RST.TySkolemVar loc pol tv) = TST.TySkolemVar loc pol defaultKind tv 
-annotTy (RST.TyUniVar loc pol tv) = TST.TyUniVar loc pol defaultKind tv
-annotTy (RST.TyRecVar loc pol tv) = TST.TyRecVar loc pol defaultKind tv
-annotTy (RST.TyData loc pol xtors) = TST.TyData loc pol defaultKind (map annotXtor xtors)
-annotTy (RST.TyCodata loc pol xtors) = TST.TyCodata loc pol defaultKind (map annotXtor xtors)
-annotTy (RST.TyDataRefined loc pol tyn xtors) = TST.TyDataRefined loc pol defaultKind tyn (map annotXtor xtors)
-annotTy (RST.TyCodataRefined loc pol tyn xtors) = TST.TyCodataRefined loc pol defaultKind tyn (map annotXtor xtors)
-annotTy (RST.TyNominal loc pol tyn vartys) = TST.TyNominal loc pol defaultKind tyn (annotVarTys vartys)
-annotTy (RST.TySyn loc pol tyn ty) = TST.TySyn loc pol tyn (annotTy ty)
-annotTy (RST.TyBot loc) = TST.TyBot loc defaultKind
-annotTy (RST.TyTop loc) =  TST.TyTop loc defaultKind
-annotTy (RST.TyUnion loc ty1 ty2) = TST.TyUnion loc defaultKind (annotTy ty1) (annotTy ty2)
-annotTy (RST.TyInter loc ty1 ty2) = TST.TyInter loc defaultKind (annotTy ty1) (annotTy ty2)
-annotTy (RST.TyRec loc pol tv ty) = TST.TyRec loc pol tv (annotTy ty)
-annotTy (RST.TyI64 loc pol) = TST.TyI64 loc pol
-annotTy (RST.TyF64 loc pol) = TST.TyF64 loc pol
-annotTy (RST.TyChar loc pol) = TST.TyChar loc pol
-annotTy (RST.TyString loc pol) = TST.TyString loc pol
-annotTy (RST.TyFlipPol pol ty) = TST.TyFlipPol pol (annotTy ty)
+annotTy :: RST.Typ pol -> Either Error (TST.Typ pol)
+annotTy (RST.TySkolemVar loc pol tv) = Right $ TST.TySkolemVar loc pol defaultKind tv 
+annotTy (RST.TyUniVar loc pol tv) = Right $ TST.TyUniVar loc pol defaultKind tv
+annotTy (RST.TyRecVar loc pol tv) = Right $ TST.TyRecVar loc pol defaultKind tv
+annotTy (RST.TyData loc pol xtors) = 
+  case collectErrs (map annotXtor xtors) of 
+    Left err -> Left err 
+    Right xtors' -> Right $ TST.TyData loc pol defaultKind xtors'
+annotTy (RST.TyCodata loc pol xtors) = 
+  case collectErrs (map annotXtor xtors) of 
+    Left err -> Left err 
+    Right xtors' -> Right $ TST.TyCodata loc pol defaultKind xtors'
+annotTy (RST.TyDataRefined loc pol tyn xtors) = 
+  case collectErrs (map annotXtor xtors) of 
+    Left err -> Left err 
+    Right xtors' -> Right $ TST.TyDataRefined loc pol defaultKind tyn xtors'
+annotTy (RST.TyCodataRefined loc pol tyn xtors) = 
+  case collectErrs (map annotXtor xtors) of 
+    Left err -> Left err 
+    Right xtors' -> Right $ TST.TyCodataRefined loc pol defaultKind tyn xtors'
+annotTy (RST.TyNominal loc pol tyn vartys) = 
+  case annotVarTys vartys of 
+    Left err -> Left err 
+    Right vartys' -> Right $ TST.TyNominal loc pol defaultKind tyn vartys'
+annotTy (RST.TySyn loc pol tyn ty) = 
+  case annotTy ty of 
+    Left err -> Left err 
+    Right ty' -> Right $ TST.TySyn loc pol tyn ty'
+annotTy (RST.TyBot loc) = Right $ TST.TyBot loc defaultKind
+annotTy (RST.TyTop loc) = Right $  TST.TyTop loc defaultKind
+annotTy (RST.TyUnion loc ty1 ty2) =
+  case annotTy ty1 of 
+    Left err -> Left err 
+    Right ty1' -> 
+      case annotTy ty2 of 
+        Left err -> Left err 
+        Right ty2' -> Right $ TST.TyUnion loc defaultKind ty1' ty2'
+annotTy (RST.TyInter loc ty1 ty2) = 
+  case annotTy ty1 of 
+    Left err -> Left err 
+    Right ty1' -> 
+      case annotTy ty2 of 
+        Left err -> Left err 
+        Right ty2' -> Right $ TST.TyInter loc defaultKind ty1' ty2' 
+annotTy (RST.TyRec loc pol tv ty) = 
+  case annotTy ty of 
+    Left err -> Left err 
+    Right ty' -> Right $ TST.TyRec loc pol tv ty'
+annotTy (RST.TyI64 loc pol) = Right $ TST.TyI64 loc pol
+annotTy (RST.TyF64 loc pol) = Right $ TST.TyF64 loc pol
+annotTy (RST.TyChar loc pol) = Right $ TST.TyChar loc pol
+annotTy (RST.TyString loc pol) = Right $ TST.TyString loc pol
+annotTy (RST.TyFlipPol pol ty) = 
+  case annotTy ty of 
+    Left err -> Left err 
+    Right ty' -> Right $ TST.TyFlipPol pol ty'
 
 
-annotateDataDecl :: RST.DataDecl -> TST.DataDecl 
+annotateDataDecl :: RST.DataDecl -> Either Error TST.DataDecl 
 annotateDataDecl RST.NominalDecl {
   data_loc = loc, 
   data_doc = doc,
@@ -124,15 +190,19 @@ annotateDataDecl RST.NominalDecl {
   data_kind = polyknd,
   data_xtors = xtors 
   } = do
-  let xtors' = Data.Bifunctor.bimap (map annotXtor) (map annotXtor) xtors
-  TST.NominalDecl { 
-    data_loc = loc, 
-    data_doc = doc,
-    data_name = tyn,
-    data_polarity = pol,
-    data_kind = polyknd,
-    data_xtors = xtors'
-  }        
+  let xtors' = Data.Bifunctor.bimap (collectErrs . map annotXtor) (collectErrs . map annotXtor) xtors
+  case xtors' of 
+    (Left err , _) -> Left err
+    (_, Left err) -> Left err 
+    (Right xtorsPol, Right xtorsNeg)-> 
+      Right TST.NominalDecl { 
+        data_loc = loc, 
+        data_doc = doc,
+        data_name = tyn,
+        data_polarity = pol,
+        data_kind = polyknd,
+        data_xtors = (xtorsPol,xtorsNeg)
+      }        
 annotateDataDecl RST.RefinementDecl { 
   data_loc = loc, 
   data_doc = doc,
@@ -146,19 +216,35 @@ annotateDataDecl RST.RefinementDecl {
   } =  do
   let empt' = Data.Bifunctor.bimap annotTy annotTy empt
   let ful' = Data.Bifunctor.bimap annotTy annotTy ful
-  let xtors' = Data.Bifunctor.bimap (map annotXtor) (map annotXtor) xtors
-  let xtorsref' = Data.Bifunctor.bimap (map annotXtor) (map annotXtor) xtorsref
-  TST.RefinementDecl {
-    data_loc = loc, 
-    data_doc = doc,
-    data_name = tyn,
-    data_polarity = pol ,
-    data_refinement_empty = empt',
-    data_refinement_full = ful',
-    data_kind = polyknd,
-    data_xtors = xtors',
-    data_xtors_refined = xtorsref' 
-  }
+  let xtors' = Data.Bifunctor.bimap (collectErrs . map annotXtor) (collectErrs . map annotXtor) xtors
+  let xtorsref' = Data.Bifunctor.bimap (collectErrs . map annotXtor) (collectErrs . map annotXtor) xtorsref
+  case empt' of 
+    (Left err,_) -> Left err 
+    (_, Left err) -> Left err 
+    (Right emptPos,Right emptNeg) -> 
+      case ful' of 
+        (Left err,_)-> Left err 
+        (_,Left err) -> Left err 
+        (Right fulPos, Right fulNeg) -> 
+          case xtors' of 
+            (Left err,_) -> Left err 
+            (_,Left err) -> Left err
+            (Right xtorsPos, Right xtorsNeg) -> 
+              case xtorsref' of 
+                (Left err,_) -> Left err
+                (_,Left err) -> Left err 
+                (Right xtorsrefPos, Right xtorsrefNeg) -> 
+                  Right TST.RefinementDecl {
+                    data_loc = loc,
+                    data_doc = doc,
+                    data_name = tyn,
+                    data_polarity = pol ,
+                    data_refinement_empty = (emptPos,emptNeg),
+                    data_refinement_full = (fulPos, fulNeg),
+                    data_kind = polyknd,
+                    data_xtors = (xtorsPos, xtorsNeg),
+                    data_xtors_refined = (xtorsrefPos, xtorsrefNeg)
+                    }
 
 --------------------------------------------------------------------------------------------
 -- checking Kinds
