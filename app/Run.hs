48d20d0fe1a2027f5f6037f70a1c7cc1e905b7a3
David Binder <david.binder@uni-tuebingen.de>
2022-09-30 16:19:17 +0200

    Remove src/Driver/Repl.hs module

--- app/Run.hs
+++ app/Run.hs
@@ -1,12 +1,11 @@
 module Run (runRun) where
 
 import Control.Monad.IO.Class (liftIO)
-import Data.Foldable (fold)
 import Data.Map qualified as M
 
 import Driver.Definition
+import Driver.Environment
 import Driver.Driver ( runCompilationModule )
-import Driver.Repl (desugarEnv)
 import Eval.Definition (EvalEnv)
 import Eval.Eval (eval)
 import Pretty.Pretty (ppPrintIO)
@@ -24,6 +23,12 @@ driverAction mn = do
   runCompilationModule mn
   queryTypecheckedModule mn
 
+desugarEnv :: Environment -> EvalEnv
+desugarEnv MkEnvironment { prdEnv, cnsEnv, cmdEnv } = (prd,cns,cmd)
+  where
+    prd = (\(tm,_,_) -> tm) <$> prdEnv
+    cns = (\(tm,_,_) -> tm) <$> cnsEnv
+    cmd = fst <$> cmdEnv
 
 runRun :: DebugFlags -> ModuleName -> IO ()
 runRun DebugFlags { df_debug, df_printGraphs } mn = do
@@ -33,7 +38,7 @@ runRun DebugFlags { df_debug, df_printGraphs } mn = do
     Left errs -> mapM_ printLocatedReport errs
     Right (_, MkDriverState { drvEnv }) -> do
       -- Run program
-      let compiledEnv :: EvalEnv = focus CBV ((\map -> fold $ desugarEnv <$> M.elems map) drvEnv)
+      let compiledEnv :: EvalEnv = focus CBV ((foldMap desugarEnv . M.elems) drvEnv)
       evalCmd <- liftIO $ eval (TST.Jump defaultLoc (MkFreeVarName "main")) compiledEnv
       case evalCmd of
           Left errs -> mapM_ printLocatedReport errs
