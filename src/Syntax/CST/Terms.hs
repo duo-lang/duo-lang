14d8a4e6c5c146243eccfb770f59e34813c09c65
Jana Ermantraut <j.ermantraut@mail.de>
2022-10-11 00:36:50 +0200

    overlap implemented, 1x Testcase added, but not tested

--- src/Syntax/CST/Terms.hs
+++ src/Syntax/CST/Terms.hs
@@ -1,29 +1,31 @@
 module Syntax.CST.Terms where
 
-import Syntax.CST.Names ( FreeVarName, XtorName, PrimName )
-import Loc ( HasLoc(..), Loc )
+import Data.Text (pack)
+import Loc (HasLoc (..), Loc, defaultLoc)
+import Syntax.CST.Names (FreeVarName (MkFreeVarName), PrimName, XtorName (MkXtorName))
 
 --------------------------------------------------------------------------------------------
--- Substitutions 
+-- Substitutions
 --------------------------------------------------------------------------------------------
 
 data TermOrStar where
-    ToSTerm :: Term -> TermOrStar
-    ToSStar :: TermOrStar
+  ToSTerm :: Term -> TermOrStar
+  ToSStar :: TermOrStar
 
 deriving instance Show TermOrStar
+
 deriving instance Eq TermOrStar
 
-newtype Substitution =
-  MkSubstitution { unSubstitution :: [Term] }
+newtype Substitution = MkSubstitution {unSubstitution :: [Term]}
 
 deriving instance Show Substitution
+
 deriving instance Eq Substitution
 
-newtype SubstitutionI =
-  MkSubstitutionI { unSubstitutionI :: [TermOrStar] }
+newtype SubstitutionI = MkSubstitutionI {unSubstitutionI :: [TermOrStar]}
 
 deriving instance Show SubstitutionI
+
 deriving instance Eq SubstitutionI
 
 --------------------------------------------------------------------------------------------
@@ -31,15 +33,104 @@ deriving instance Eq SubstitutionI
 --------------------------------------------------------------------------------------------
 
 data Pattern where
-  PatXtor     :: Loc -> XtorName -> [Pattern] -> Pattern
-  PatVar      :: Loc -> FreeVarName -> Pattern
-  PatStar     :: Loc -> Pattern
+  PatXtor :: Loc -> XtorName -> [Pattern] -> Pattern
+  PatVar :: Loc -> FreeVarName -> Pattern
+  PatStar :: Loc -> Pattern
   PatWildcard :: Loc -> Pattern
 
-overlap :: [Pattern] -> Maybe String 
-overlap = undefined
+--------------------------------------------
+
+test1 :: [Pattern]
+test1 =
+  [ PatXtor defaultLoc (MkXtorName (pack "Leaf")) [PatVar defaultLoc (MkFreeVarName (pack "x"))],
+    PatXtor
+      defaultLoc
+      (MkXtorName (pack "Branch"))
+      [ PatXtor defaultLoc (MkXtorName (pack "Leaf")) [PatVar defaultLoc (MkFreeVarName (pack "y"))],
+        PatVar defaultLoc (MkFreeVarName (pack "t2"))
+      ],
+    PatVar defaultLoc (MkFreeVarName (pack "x")),
+    PatStar defaultLoc,
+    PatWildcard defaultLoc,
+    PatXtor
+      defaultLoc
+      (MkXtorName (pack "Branch"))
+      [ PatXtor defaultLoc (MkXtorName (pack "Leaf")) [PatVar defaultLoc (MkFreeVarName (pack "y"))],
+        PatXtor defaultLoc (MkXtorName (pack "Leaf")) [PatVar defaultLoc (MkFreeVarName (pack "z"))]
+      ],
+    PatXtor
+      defaultLoc
+      (MkXtorName (pack "Branch"))
+      [ PatVar defaultLoc (MkFreeVarName (pack "t1")),
+        PatVar defaultLoc (MkFreeVarName (pack "t2"))
+      ],
+    PatVar defaultLoc (MkFreeVarName (pack "t"))
+  ]
+
+--An Overlap Message is a String
+type OverlapMsg = String
+
+--An Overlap may be an Overlap Message.
+type Overlap = Maybe OverlapMsg
+
+--Generates the Overlap of Patterns between one another.
+overlap :: [Pattern] -> Overlap
+overlap [] = Nothing
+overlap (x : xs) =
+  let xOverlaps = map (overlapA2 x) xs
+   in concatOverlaps $ xOverlaps ++ [overlap xs]
+  where
+    --reduces multiple potential Overlap Messages into one potential Overlap Message.
+    concatOverlaps :: [Overlap] -> Overlap
+    concatOverlaps xs =
+      let concatRule = \x y -> x ++ "\n \n \n" ++ y
+       in foldr (liftm2 concatRule) Nothing xs
+      where
+        liftm2 :: (a -> a -> a) -> Maybe a -> Maybe a -> Maybe a
+        liftm2 _ x Nothing = x
+        liftm2 _ Nothing y = y
+        liftm2 f (Just x) (Just y) = Just $ (f x y)
+
+    --generates an Overlap Message for patterns p1 p2.
+    overlapMsg :: Pattern -> Pattern -> OverlapMsg
+    overlapMsg p1 p2 =
+      let p1Str = patternToStr p1
+          p2Str = patternToStr p2
+       in "Overlap found: \n" ++ p1Str ++ " overlaps with " ++ p2Str ++ "\n"
+
+    patternToStr :: Pattern -> String
+    patternToStr (PatVar loc varName) = "Variable Pattern " ++ (show varName) ++ "in: " ++ (show loc)
+    patternToStr (PatStar loc) = "* Pattern in: " ++ (show loc)
+    patternToStr (PatWildcard loc) = "Wildcard Pattern in: " ++ (show loc)
+    patternToStr (PatXtor loc xtorName _) = "Constructor Pattern " ++ (show xtorName) ++ "in: " ++ (show loc)
+
+    --determines for 2x Patterns p1 p2 a potential Overlap message on p1 'containing' p2 or p2 'containing' p1.
+    overlapA2 :: Pattern -> Pattern -> Overlap
+    --Case 1: p1 is a Constructor Pattern.
+    overlapA2 p1@(PatXtor _ xXtorName xPatterns) p2 =
+      case p2 of
+        --Constructor pattern p1 can only potentially overlap with another Constructor pattern of the same Name.
+        (PatXtor _ yXtorName yPatterns)
+          | (xXtorName == yXtorName) ->
+            let subPatternsOverlaps = (map overlapA2 xPatterns) <*> yPatterns
+                subPatternsOverlap = concatOverlaps subPatternsOverlaps
+             in case subPatternsOverlap of
+                  Nothing -> Nothing
+                  (Just subPatternsOverlapMsg) ->
+                    Just $
+                      (overlapMsg p1 p2)
+                        ++ "due to the following Subpattern Overlap:"
+                        ++ "\n"
+                        ++ subPatternsOverlapMsg
+        -- ...and cannot overlap otherwise.
+        _ -> Nothing
+    --Case2: p1 is any other Pattern -> p1 already overlaps with p2!
+    overlapA2 p1 p2 = Just $ overlapMsg p1 p2
+
+--------------------------------------------
 
 deriving instance Show Pattern
+
 deriving instance Eq Pattern
 
 instance HasLoc Pattern where
@@ -52,13 +143,14 @@ instance HasLoc Pattern where
 -- Cases/Cocases
 --------------------------------------------------------------------------------------------
 
-data TermCase  = MkTermCase
-  { tmcase_loc  :: Loc
-  , tmcase_pat  :: Pattern
-  , tmcase_term :: Term
+data TermCase = MkTermCase
+  { tmcase_loc :: Loc,
+    tmcase_pat :: Pattern,
+    tmcase_term :: Term
   }
 
 deriving instance Show TermCase
+
 deriving instance Eq TermCase
 
 instance HasLoc TermCase where
@@ -75,27 +167,29 @@ data NominalStructural where
   deriving (Eq, Ord, Show)
 
 data Term where
-    PrimTerm :: Loc -> PrimName -> Substitution -> Term 
-    Var :: Loc -> FreeVarName -> Term
-    Xtor :: Loc -> XtorName -> SubstitutionI -> Term
-    Semi :: Loc -> XtorName -> SubstitutionI -> Term -> Term
-    Case :: Loc -> [TermCase] -> Term
-    CaseOf :: Loc -> Term -> [TermCase] -> Term
-    Cocase :: Loc -> [TermCase] -> Term
-    CocaseOf :: Loc -> Term -> [TermCase] -> Term
-    MuAbs :: Loc -> FreeVarName -> Term -> Term
-    Dtor :: Loc -> XtorName -> Term -> SubstitutionI -> Term
-    PrimLitI64 :: Loc -> Integer -> Term
-    PrimLitF64 :: Loc -> Double -> Term
-    PrimLitChar :: Loc -> Char -> Term
-    PrimLitString :: Loc -> String -> Term
-    NatLit :: Loc -> NominalStructural -> Int -> Term
-    TermParens :: Loc -> Term -> Term
-    FunApp :: Loc -> Term -> Term -> Term
-    Lambda :: Loc -> FreeVarName -> Term -> Term
-    CoLambda :: Loc -> FreeVarName -> Term -> Term
-    Apply :: Loc -> Term -> Term -> Term 
+  PrimTerm :: Loc -> PrimName -> Substitution -> Term
+  Var :: Loc -> FreeVarName -> Term
+  Xtor :: Loc -> XtorName -> SubstitutionI -> Term
+  Semi :: Loc -> XtorName -> SubstitutionI -> Term -> Term
+  Case :: Loc -> [TermCase] -> Term
+  CaseOf :: Loc -> Term -> [TermCase] -> Term
+  Cocase :: Loc -> [TermCase] -> Term
+  CocaseOf :: Loc -> Term -> [TermCase] -> Term
+  MuAbs :: Loc -> FreeVarName -> Term -> Term
+  Dtor :: Loc -> XtorName -> Term -> SubstitutionI -> Term
+  PrimLitI64 :: Loc -> Integer -> Term
+  PrimLitF64 :: Loc -> Double -> Term
+  PrimLitChar :: Loc -> Char -> Term
+  PrimLitString :: Loc -> String -> Term
+  NatLit :: Loc -> NominalStructural -> Int -> Term
+  TermParens :: Loc -> Term -> Term
+  FunApp :: Loc -> Term -> Term -> Term
+  Lambda :: Loc -> FreeVarName -> Term -> Term
+  CoLambda :: Loc -> FreeVarName -> Term -> Term
+  Apply :: Loc -> Term -> Term -> Term
+
 deriving instance Show Term
+
 deriving instance Eq Term
 
 instance HasLoc Term where
@@ -117,5 +211,5 @@ instance HasLoc Term where
   getLoc (FunApp loc _ _) = loc
   getLoc (Lambda loc _ _) = loc
   getLoc (CoLambda loc _ _) = loc
-  getLoc (Apply loc _ _) = loc 
-  getLoc (PrimTerm loc _ _) = loc 
+  getLoc (Apply loc _ _) = loc
+  getLoc (PrimTerm loc _ _) = loc
