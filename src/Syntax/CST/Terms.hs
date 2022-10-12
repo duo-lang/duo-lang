9ee37f26bcbfb2212a3330d7d3a982e27f2d270f
Jana Ermantraut <j.ermantraut@mail.de>
2022-10-12 21:19:11 +0200

    added descriptions of expectation to testcases 1-8

--- src/Syntax/CST/Terms.hs
+++ src/Syntax/CST/Terms.hs
@@ -40,6 +40,38 @@ data Pattern where
 
 --------------------------------------------
 
+
+-- (1) Leaf x 
+-- (2) Branch (Leaf y) t2 
+-- (3) x
+-- (4) *
+-- (5) _
+-- (6) Branch (Leaf y) (Leaf z)
+-- (7) Branch t1 t2 
+-- (8) t
+-- -> Overlap expected between:
+--    (1) and (3)
+--    (1) and (4)
+--    (1) and (5)
+--    (1) and (8)
+--    (2) and (3)
+--    (2) and (4)
+--    (2) and (5)
+--    (2) and (6) due to Subpattern matches of (Leaf y) and (Leaf y), t2 and (Leaf z)
+--    (2) and (7) due to Subpattern matches of (Leaf y) and t1, t2 and t2
+--    (2) and (8)
+--    (3) and (4)
+--    (3) and (5)
+--    (3) and (6)
+--    (3) and (7)
+--    (3) and (8)
+--    (4) and (5)
+--    (4) and (6)
+--    (4) and (7)
+--    (4) and (8)
+--    (6) and (7) due to Subpattern matches of (Leaf y) and t1, (Leaf z) and t2
+--    (6) and (8)
+--    (7) and (8)
 test1 :: [Pattern]
 test1 =
   [ PatXtor defaultLoc (MkXtorName (pack "Leaf")) [PatVar defaultLoc (MkFreeVarName (pack "x"))],
@@ -82,7 +114,6 @@ test1 =
 --    (2) and (5)
 --    (3) and (5)
 --    (4) and (5)
-
 test2 :: [Pattern]
 test2 =
   [ PatVar defaultLoc (MkFreeVarName (pack "m")),
@@ -100,17 +131,25 @@ test3 = []
 test4 :: [Pattern]
 test4 = [PatStar defaultLoc]
 
+-- (1) Node y Empty (Node z Empty Empty)
+-- (2) Node z Empty Empty
 -- No Overlap expected.
 test5 :: [Pattern]
 test5 =
   [ PatXtor
       defaultLoc
       (MkXtorName (pack "Node"))
-      [ PatXtor defaultLoc (MkXtorName (pack "Subtree")) [PatVar defaultLoc (MkFreeVarName (pack "y"))],
-        PatXtor defaultLoc (MkXtorName (pack "Subtree")) [PatVar defaultLoc (MkFreeVarName (pack "x"))],
-        PatVar defaultLoc (MkFreeVarName (pack "z"))
-      ]
-  ]
+      [ PatVar defaultLoc (MkFreeVarName (pack "y")),
+        PatXtor defaultLoc (MkXtorName (pack "Empty")) [],
+        PatXtor defaultLoc (MkXtorName (pack "Node")) [ PatVar defaultLoc (MkFreeVarName (pack "z")),
+                                                        PatXtor defaultLoc (MkXtorName (pack "Empty")) [],
+                                                        PatXtor defaultLoc (MkXtorName (pack "Empty")) []]],
+    PatXtor
+      defaultLoc
+      (MkXtorName (pack "Node"))
+      [ PatVar defaultLoc (MkFreeVarName (pack "z")),
+        PatXtor defaultLoc (MkXtorName (pack "Empty")) [],
+        PatXtor defaultLoc (MkXtorName (pack "Empty")) []]]
 
 -- (1) x
 -- (2) z
@@ -126,13 +165,10 @@ test6 =
     PatVar defaultLoc (MkFreeVarName (pack "x"))
   ]
 
--- (1) (x:(y:(z:zs)))
--- (2) (x:(y:(z:(m:ms))))
--- bzw.
 -- (1) Cons x (Cons y (Cons z zs))
 -- (2) Cons x (Cons y (Cons z (Cons m ms)))
 -- -> Overlap expected between:
--- (1) and (2) (due to Subpattern Overlap between x and x, (Cons y (Cons z zs)) and (Cons y (Cons z (Cons m ms))))
+--    (1) and (2) (due to Subpattern Overlap between x and x, (Cons y (Cons z zs)) and (Cons y (Cons z (Cons m ms))))
 test7 :: [Pattern]
 test7 = [PatXtor defaultLoc (MkXtorName (pack "Cons")) 
           [ PatVar defaultLoc (MkFreeVarName (pack "x")),
@@ -147,6 +183,9 @@ test7 = [PatXtor defaultLoc (MkXtorName (pack "Cons"))
               [PatVar defaultLoc (MkFreeVarName (pack "z")),
                PatXtor defaultLoc (MkXtorName (pack "Cons")) [PatVar defaultLoc (MkFreeVarName (pack "m")), PatVar defaultLoc (MkFreeVarName (pack "ms"))]]]]]
 
+-- (1) Branch (Leaf x) (Leaf y)
+-- (2) Branch (Leaf x) (Branch (Leaf y1) (Leaf y2))
+-- No Overlap expected.
 test8 :: [Pattern]
 test8 = [PatXtor
           defaultLoc
@@ -165,7 +204,6 @@ test8 = [PatXtor
               PatXtor defaultLoc (MkXtorName (pack "Leaf")) [PatVar defaultLoc (MkFreeVarName (pack "y2"))]
             ]
           ]]
-
 
 -- | An Overlap Message is a String
 type OverlapMsg = String
