eddabe72e5cdcf6ad386fc82b31eccdf689be4c4
MarcoTz <marco.tzschentke@gmail.com>
2022-10-19 12:08:24 +0200

    changed tvar to tvarAnnot

--- src/Parser/Types.hs
+++ src/Parser/Types.hs
@@ -108,7 +108,7 @@ xdataTypeP Codata = do
 typeVariableP :: Parser (Typ, SourcePos)
 typeVariableP = do
   startPos <- getSourcePos
-  (tvar, endPos) <- tvarP
+  (tvar, mk, endPos) <- tvarAnnotP
   sc
   pure (TySkolemVar (Loc startPos endPos) tvar, endPos)
 
@@ -117,7 +117,7 @@ recTypeP = do
   startPos <- getSourcePos
   _ <- keywordP KwRec
   sc
-  (rv,_) <- tvarP
+  (rv,mk,_) <- tvarAnnotP
   symbolP SymDot
   sc
   (ty, endPos) <- typP
