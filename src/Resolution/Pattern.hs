fbe85c5a2b2a4f3652c446857efb93a2538bbab9
David Binder <david.binder@uni-tuebingen.de>
2022-10-20 15:43:34 +0200

    Add pattern resolution for Patterns and StarPatterns

--- src/Resolution/Pattern.hs
+++ src/Resolution/Pattern.hs
@@ -18,6 +18,7 @@ import Syntax.CST.Types qualified as CST
 import Syntax.CST.Types (PrdCns(..), PrdCnsRep(..))
 import Syntax.CST.Names
 import Loc ( Loc, HasLoc(getLoc))
+import Data.Either (isRight, fromLeft, fromRight)
 
 ---------------------------------------------------------------------------------
 -- Resolved Pattern
@@ -27,25 +28,48 @@ import Loc ( Loc, HasLoc(getLoc))
 ---------------------------------------------------------------------------------
 
 data Pattern where
-  PatXtor        :: Loc -> PrdCns -> CST.NominalStructural -> XtorName -> [Pattern] -> Pattern
-  PatVar         :: Loc -> PrdCns -> FreeVarName -> Pattern
-  PatStar        :: Loc -> PrdCns -> Pattern
-  PatWildcard    :: Loc -> PrdCns -> Pattern
+  PatXtor     :: Loc -> PrdCns -> CST.NominalStructural -> XtorName -> [Pattern] -> Pattern
+  PatVar      :: Loc -> PrdCns -> FreeVarName -> Pattern
+  PatWildcard :: Loc -> PrdCns -> Pattern
+
+deriving instance Eq Pattern
+deriving instance Show Pattern
 
 instance HasLoc Pattern where
   getLoc :: Pattern -> Loc
   getLoc (PatXtor loc _ _ _ _) = loc
   getLoc (PatVar loc _ _) = loc
-  getLoc (PatStar loc _) = loc
   getLoc (PatWildcard loc _) = loc
+
+data StarPattern where
+  PatStar     :: Loc -> PrdCns -> StarPattern
+  PatXtorStar :: Loc -> PrdCns -> CST.NominalStructural -> XtorName -> ([Pattern],StarPattern,[Pattern]) -> StarPattern
+
+deriving instance Eq StarPattern
+deriving instance Show StarPattern
+
+instance HasLoc StarPattern where
+  getLoc :: StarPattern -> Loc
+  getLoc (PatStar loc _) = loc
+  getLoc (PatXtorStar loc _ _ _ _) = loc
 
 ---------------------------------------------------------------------------------
 -- Resolve Pattern
 ---------------------------------------------------------------------------------
 
+findAtMostOneRight :: HasLoc b => [Either a b] -> ResolverM (Either [a] ([a],b,[a]))
+findAtMostOneRight args = case break isRight args of
+  (pats, []) -> pure $ Left (fromLeft undefined <$> pats)
+  (left_pats, (starpat: right_pats)) ->
+    case break isRight right_pats of
+      (right_pats, []) -> pure $ Right (fromLeft undefined <$> left_pats,fromRight undefined starpat,fromLeft undefined <$> right_pats)
+      (_, _:_) -> throwOtherError (getLoc (fromRight undefined starpat)) ["Found more than one star in pattern"]
+      
+    
+
 -- | Annotate every part of the pattern with information on whether it stands for
 -- a producer or consumer.
-resolvePattern :: PrdCns -> CST.Pattern -> ResolverM Pattern
+resolvePattern :: PrdCns -> CST.Pattern -> ResolverM (Either Pattern StarPattern)
 resolvePattern pc (CST.PatXtor loc xt pats) = do
   -- Lookup up the arity information in the symbol table.
   (_,res) <- lookupXtor loc xt
@@ -62,19 +86,22 @@ resolvePattern pc (CST.PatXtor loc xt pats) = do
         (Prd, CST.Data  ) -> pure ()
         (Cns, CST.Codata) -> pure ()
       pats' <- zipWithM resolvePattern arity pats
-      pure (PatXtor loc pc ns xt pats')
+      pats'' <- findAtMostOneRight pats'
+      case pats'' of
+        Left pats''' -> pure $ Left (PatXtor loc pc ns xt pats''')
+        Right pats''' -> pure $ Right (PatXtorStar loc pc ns xt pats''')
 resolvePattern Prd (CST.PatVar loc var@(MkFreeVarName name)) = do
   when ("k" `T.isPrefixOf` name) $
     tell [MisnamedProducerVar loc name]
-  pure $ PatVar loc Prd var
+  pure $ Left (PatVar loc Prd var)
 resolvePattern Cns (CST.PatVar loc var@(MkFreeVarName name))  = do
   unless ("k" `T.isPrefixOf` name) $
     tell [MisnamedConsumerVar loc name]
-  pure $ PatVar loc Cns var
+  pure $ Left (PatVar loc Cns var)
 resolvePattern pc (CST.PatStar loc) = do
-  pure $ PatStar loc pc
+  pure $ Right (PatStar loc pc)
 resolvePattern pc (CST.PatWildcard loc) = do
-  pure $ PatWildcard loc pc
+  pure $ Left (PatWildcard loc pc)
 
 ---------------------------------------------------------------------------------
 -- Analyze Patterns
@@ -85,48 +112,48 @@ data AnalyzedPattern
   | ImplicitPrdPattern Loc XtorName ([(Loc, PrdCns, FreeVarName)], PrdCnsRep Prd,[(Loc, PrdCns,FreeVarName)])
   | ImplicitCnsPattern Loc XtorName ([(Loc, PrdCns, FreeVarName)], PrdCnsRep Cns,[(Loc, PrdCns,FreeVarName)])
 
-isStar :: Pattern -> Bool
-isStar (PatStar _ _) = True
-isStar _ = False
 
 fromVar :: Pattern -> ResolverM (Loc, PrdCns, FreeVarName)
 fromVar (PatVar loc pc var) = pure (loc, pc, var)
 fromVar pat = throwOtherError (getLoc pat) ["Called function \"fromVar\" on pattern which is not a variable."]
 
+
 analyzePattern :: CST.DataCodata -> CST.Pattern -> ResolverM AnalyzedPattern
 analyzePattern dc pat = do
   pat' <- resolvePattern (case dc of CST.Data -> Prd; CST.Codata -> Cns) pat
   case pat' of
-    PatXtor loc _pc _ns xt pats -> do
-      case length (filter isStar pats) of
-        0 -> do
-          vars <- sequence (fromVar <$> pats)
-          pure $ ExplicitPattern loc xt vars
-        1 -> do
-          case break isStar pats of
-            (args1,PatStar _ pc:args2) -> do
-              args1' <- sequence (fromVar <$> args1)
-              args2' <- sequence (fromVar <$> args2)
-              case pc of
-                Cns -> pure $ ImplicitPrdPattern loc xt (args1', PrdRep, args2')
-                Prd -> pure $ ImplicitCnsPattern loc xt (args1', CnsRep, args2')
-            (_,_) -> throwOtherError loc ["Not reachable, since number \"1\" was returned in the check."]
-        n -> throwError $ ErrResolution (InvalidStar loc ("More than one star used in binding site: " <> T.pack (show n) <> " stars used.")) :| []
-    _ -> throwOtherError (getLoc pat) ["Invalid pattern in function \"analyzePattern\""]
+    -- Patterns
+    Left (PatXtor loc _pc _ns xt pats) -> do
+      vars <- mapM fromVar pats
+      pure $ ExplicitPattern loc xt vars
+    Left _ -> throwOtherError (getLoc pat) ["Invalid pattern in function \"analyzePattern\""]
+    -- StarPatterns
+    Right (PatXtorStar loc _pc _ns xt (pats_left,PatStar _ pc,pats_right)) -> do
+      pats_left' <- mapM fromVar pats_left
+      pats_right' <- mapM fromVar pats_right
+      case pc of
+        Cns -> pure $ ImplicitPrdPattern loc xt (pats_left', PrdRep, pats_right')
+        Prd -> pure $ ImplicitCnsPattern loc xt (pats_left', CnsRep, pats_right')
+    Right _ -> throwOtherError (getLoc pat) ["Invalid star pattern in function \"analyzePattern\""]
 
 
 analyzeInstancePattern :: CST.Pattern -> ResolverM (Loc, XtorName, [(Loc, PrdCns, FreeVarName)])
 analyzeInstancePattern (CST.PatXtor loc xt pats) = do
   (_,res) <- lookupXtor loc xt
-  case res of 
+  case res of
     XtorNameResult {} -> do
       throwOtherError loc ["Expected typeclass method but found xtor" <> T.pack (show xt)]
     MethodNameResult _cn arity -> do
       when (length arity /= length pats) $
         throwError (ErrResolution (XtorArityMismatch loc xt (length arity) (length pats)) :| [])
       pats' <- zipWithM resolvePattern arity pats
-      args <- mapM fromVar pats'
-      pure (loc, xt, args)
-analyzeInstancePattern pat = 
+      foo <- findAtMostOneRight pats'
+      case foo of
+        Left pats2 -> do
+          args <- mapM fromVar pats2
+          pure (loc, xt, args)
+        Right _ ->
+          throwOtherError loc ["Found star in instance method"]
+analyzeInstancePattern pat =
   throwOtherError (getLoc pat) ["Expected typeclass method but found pattern" <> T.pack (show pat)]
 
