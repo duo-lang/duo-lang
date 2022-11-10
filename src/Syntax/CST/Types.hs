0993b03d0fa2a02f0c609a8748b6b9807675f3d5
MarcoTz <marco.tzschentke@gmail.com>
2022-11-10 13:27:39 +0100

    added TyKindAnnot

--- src/Syntax/CST/Types.hs
+++ src/Syntax/CST/Types.hs
@@ -51,6 +51,7 @@ data Typ where
   -- should never be directly constructed elsewhere.
   TyBinOp :: Loc -> Typ -> BinOp -> Typ -> Typ
   TyParens :: Loc -> Typ -> Typ
+  TyKindAnnot :: Loc -> MonoKind -> Typ -> Typ
   deriving Show
 
 instance HasLoc Typ where
@@ -70,6 +71,7 @@ instance HasLoc Typ where
   getLoc (TyBinOpChain ty _) = getLoc ty
   getLoc (TyBinOp loc _ _ _) = loc
   getLoc (TyParens loc _) = loc
+  getLoc (TyKindAnnot loc _ _) = loc
 
 
 data XtorSig = MkXtorSig
