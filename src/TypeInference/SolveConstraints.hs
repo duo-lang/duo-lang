99d39b9ea03cfbff724f5432587e5fd1e3d87ce3
MarcoTz <marco.tzschentke@gmail.com>
2022-12-02 13:16:31 +0100

    refactored TyApp TyNominal in solveConstraints

--- src/TypeInference/SolveConstraints.hs
+++ src/TypeInference/SolveConstraints.hs
@@ -351,12 +351,14 @@ subConstraints (SubType _ (TyCodataRefined _ PosRep _ tn1 dtors1) (TyCodataRefin
 --     Bool <: Nat               ~>     FAIL
 --     Bool <: Bool              ~>     []
 --
-subConstraints (SubType _ (TyApp _ _ (TyNominal _ _ _ tn1) args1) (TyApp _ _ (TyNominal _ _ _ tn2) args2)) | tn1 == tn2 = do
-    let f (CovariantType ty1) (CovariantType ty2) = SubType NominalSubConstraint ty1 ty2
-        f (ContravariantType ty1) (ContravariantType ty2) = SubType NominalSubConstraint ty2 ty1
-        f _ _ = error "cannot occur"
-        constraints = NE.toList $ NE.zipWith f args1 args2
-    pure (DataNominal tn1 $ SubVar . void <$> constraints, constraints)
+subConstraints (SubType info (TyApp _ _ ty1@(TyNominal _ _ _ tn1) args1) (TyApp _ _ ty2@TyNominal{} args2)) = do
+  let 
+    f (CovariantType ty1) (CovariantType ty2) = SubType NominalSubConstraint ty1 ty2
+    f (ContravariantType ty1) (ContravariantType ty2) = SubType NominalSubConstraint ty2 ty1
+    f _ _ = error "cannot occur"
+    nomConstr = SubType info ty1 ty2
+    constraints = nomConstr : (NE.toList $ NE.zipWith f args1 args2)
+  pure (DataNominal tn1 $ SubVar . void <$> constraints, constraints)
 subConstraints (SubType _ t1@(TyNominal _ _ _ tn1) t2@(TyNominal _ _ _ tn2)) = 
   if tn1==tn2 then 
     pure (Refl t1 t2, [])
