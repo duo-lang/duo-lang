8a675498a2dbcc7dd36d60804d9ae37e42de3c70
pflotscher <leon.pfletschinger@gmail.com>
2023-03-27 17:48:13 +0200

    create new branch

--- src/Xfunc/Xfunc.hs
+++ src/Xfunc/Xfunc.hs
@@ -6,3 +6,8 @@ import Data.Set qualified as Set
 
 transformable :: TST.DataDecl -> Bool
 transformable decl = Set.size (allTypeVars (decl.data_kind)) == 0
+
+
+-- xfunc a (co)datatype in a module
+xfunc :: TST.Module -> TST.DataDecl -> TST.Module
+xfunc = undefined
